3398c94286243a69eeb6166b7f0119070c6390bb
Ikeno Masaki <leafclover64@gmail.com>
2017-12-05 00:46:31 +0900

    implement update method

--- model/messages.go
+++ model/messages.go
@@ -1,19 +1,38 @@
 package model
 
+import (
+	"fmt"
+)
 
 type Messages struct {
-	Id string `xorm:char(36) pk`
-	UserId string `xorm:char(36) not null`
+	Id        string `xorm:char(36) pk`
+	UserId    string `xorm:char(36) not null`
 	ChannelId string `xorm:char(36)`
-	text string `xorm:text not null`
-	IsShared bool `xorm:bool not null`
-	IsDeleted bool `xorm:bool not null`
+	text      string `xorm:text not null`
+	IsShared  bool   `xorm:bool not null`
+	IsDeleted bool   `xorm:bool not null`
 	CreatedAt string `xorm:created not null`
 	UpdaterId string `xorm:char(36) not null`
 	UpdatedAt string `xorm:updated not null`
 }
 
-func (self *Messages) Create() error {
+// Create inserts message object.
+func (message *Messages) Create() error {
+	if message.UserId == "" {
+		return fmt.Errorf("UserId is empty")
+	}
+
+	if message.text == "" {
+		return fmt.Errorf("Text is empty")
+	}
+
+	message.Id = CreateUUID()
+	message.IsDeleted = false
+	message.UpdaterId = message.UserId
+
+	if _, err := db.Insert(message); err != nil {
+		return fmt.Errorf("Failed to create message object: %v", err)
+	}
 	return nil
 }
 
@@ -25,10 +44,27 @@ func GetMessagesFromChannel(channelId string) ([]*Messages, error) {
 	return nil, nil
 }
 
-func GetMessage(messageId string) (*Message, error) {
+func GetMessage(messageId string) (*Messages, error) {
 	return nil, nil
 }
 
+// DeleteMessage deletes the message.
 func DeleteMessage(messageId string) error {
+	var message Messages
+	has, err := db.ID(messageId).Get(&message)
+
+	if err != nil {
+		return fmt.Errorf("Failed to find message: %v", err)
+	}
+	if has == false {
+		return fmt.Errorf("MessageId is wrong")
+	}
+
+	message.IsDeleted = true
+
+	if _, err := db.ID(messageId).Update(&message); err != nil {
+		return fmt.Errorf("Failed to update message: %v", err)
+	}
+
 	return nil
 }
