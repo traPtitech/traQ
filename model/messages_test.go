47228945aa5dfe613eb2a556bfca7f54d3fd5875
Ikeno Masaki <leafclover64@gmail.com>
2017-12-06 23:55:36 +0900

    add test files (no impl)

---
+++ model/messages_test.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"testing"
+)
+
+func TestCreateMessage(t *testing.T) {
+
+}
+
+func TestUpdateMessage(t *testing.T) {
+	
+}
+
+func TestGetMessagesFromChannel(t *testing.T) {
+	
+}
+
+func TestGetMessage(t *testing.T) {
+	
+}
+
+func TestDeleteMessage(t *testing.T) {
+	
+}
---
+++ router/messages_test.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"testing"
+)
+
+func TestGetMessageByIdHandler(t *testing.T) {
+
+}
+
+func TestMessageByChannelIdHandler(t *testing.T) {
+	
+}
+
+func TestPostMessageHandler(t *testing.T) {
+
+}
+
+func TestPutMessageByIdHandler(t *testing.T) {
+
+}
+
+func TestDeleteMessageByIdHandler(t *testing.T) {
+
+}
