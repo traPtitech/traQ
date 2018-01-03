64b0c17dc178e6e7150076dcfa8f5bf0c1372e12
Ikeno Masaki <leafclover64@gmail.com>
2018-01-03 20:34:54 +0900

    modify test code

--- router/middleware_test.go
+++ router/middleware_test.go
@@ -16,28 +16,7 @@ func TestGetUserInfo(t *testing.T) {
 	c, rec := getContext(e, t, cookie, nil)
 	c.SetPath("/test")
 
-	// userの追加
-	user := &model.User{
-		Name:  "middlewareTest",
-		Email: "middleware@trap.jp",
-		Icon:  "empty",
-	}
-	if err := user.SetPassword("test"); err != nil {
-		t.Fatal(err)
-	}
-	if err := user.Create(); err != nil {
-		t.Fatal(err)
-	}
-
-	sess, err := session.Get("sessions", c)
-	if err != nil {
-		t.Fatalf("an error occurred while getting session: %v", err)
-	}
-
-	sess.Values["userID"] = user.ID
-	sess.Save(c.Request(), c.Response())
-
-	requestWithContext(t, mw(GetUserInfo(testGetUser)), c)
+	requestWithContext(t, mw(prepareUser(GetUserInfo(testGetUser))), c)
 
 	if rec.Code != http.StatusOK {
 		t.Log(rec.Code)
@@ -45,8 +24,34 @@ func TestGetUserInfo(t *testing.T) {
 	}
 }
 
+// userデータをセッションに保存する
+func prepareUser(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		user := &model.User{
+			Name:  "middlewareTest",
+			Email: "middleware@trap.jp",
+			Icon:  "empty",
+		}
+		if err := user.SetPassword("test"); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "an error occurred while setting password: %v", err)
+		}
+		if err := user.Create(); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "an error occurred while creating user: %v", err)
+		}
+
+		sess, err := session.Get("sessions", c)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "an error occurred while getting session: %v", err)
+		}
+
+		sess.Values["userID"] = user.ID
+		sess.Save(c.Request(), c.Response())
+		return next(c)
+	}
+}
+
 func testGetUser(c echo.Context) error {
-	user := c.Get("userID").(model.User)
+	user := c.Get("userID").(*model.User)
 
 	type TestResponseUser struct {
 		ID   string `json:"ID"`
