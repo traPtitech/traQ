0ac1c3038320067954c2d4f5a499a7b458e3ebbb
masutech16 <leafclover64@gmail.com>
2018-02-19 15:32:28 +0900

    file model

---
+++ model/files_test.go
@@ -0,0 +1,68 @@
+package model
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFile_TableName(t *testing.T) {
+	assert.Equal(t, "files", (&File{}).TableName())
+}
+
+func TestFile_Create(t *testing.T) {
+	beforeTest(t)
+	assert := assert.New(t)
+	writeData := bytes.NewReader(([]byte)("test message"))
+
+	assert.Error((&File{}).Create(writeData))
+	assert.Error((&File{ID: CreateUUID()}).Create(writeData))
+
+	file := &File{
+		Name:      "testFile.txt",
+		Size:      writeData.Size(),
+		CreatorID: testUserID,
+	}
+	if assert.NoError(file.Create(writeData)) {
+		assert.NotEmpty(file.ID)
+		_, err := os.Stat(dirName + "/" + file.ID)
+		assert.NoError(err)
+	}
+}
+
+func TestFile_Delete(t *testing.T) {
+	beforeTest(t)
+	assert := assert.New(t)
+
+	file := mustMakeFile(t)
+	file.IsDeleted = true
+
+	assert.NoError(file.Delete())
+}
+
+func TestGetFileByID(t *testing.T) {
+	beforeTest(t)
+	assert := assert.New(t)
+
+	file := mustMakeFile(t)
+	b, err := GetFileByID(file.ID)
+	if assert.NoError(err) {
+		assert.Equal("test message", string(b))
+	}
+}
+
+func TestGetMetaFileDataByID(t *testing.T) {
+	beforeTest(t)
+	assert := assert.New(t)
+
+	file := mustMakeFile(t)
+	result, err := GetMetaFileDataByID(file.ID)
+	if assert.NoError(err) {
+		assert.Equal(file.ID, result.ID)
+	}
+
+	_, err = GetMetaFileDataByID("wrongID")
+	assert.Error(err)
+}
