00b774dad761e1a793c73c11634aac61ccc40930
takashi <30363887+wtks@users.noreply.github.com>
2018-03-11 15:41:26 +0900

    generate thumbnails

--- model/files.go
+++ model/files.go
@@ -1,9 +1,21 @@
 package model
 
 import (
+	"bytes"
+	"context"
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"github.com/labstack/gommon/log"
+	"golang.org/x/image/bmp"
+	"golang.org/x/image/draw"
+	"golang.org/x/image/webp"
+	"golang.org/x/sync/errgroup"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"mime"
 	"os"
@@ -11,9 +23,22 @@ import (
 	"time"
 )
 
-var fileManagers = map[string]FileManager{
-	"": NewDevFileManager(),
-}
+const (
+	thumbnailMaxWidth  = 360
+	thumbnailMaxHeight = 480
+	thumbnailRatio     = float64(thumbnailMaxWidth) / float64(thumbnailMaxHeight)
+)
+
+var (
+	fileManagers = map[string]FileManager{
+		"": NewDevFileManager(),
+	}
+
+	// ErrFileThumbUnsupported : fileエラー この形式のファイルのサムネイル生成はサポートされていない
+	ErrFileThumbUnsupported = errors.New("generating a thumbnail of the file is not supported")
+	// ErrFileUnknownManager : fileエラー 不明なファイルマネージャー
+	ErrFileUnknownManager = errors.New("unknown file manager")
+)
 
 // FileManager ファイルを読み書きするマネージャーのインターフェース
 type FileManager interface {
@@ -29,15 +54,18 @@ type FileManager interface {
 
 // File DBに格納するファイルの構造体
 type File struct {
-	ID        string    `xorm:"char(36) pk"`
-	Name      string    `xorm:"text not null"`
-	Mime      string    `xorm:"text not null"`
-	Size      int64     `xorm:"bigint not null"`
-	CreatorID string    `xorm:"char(36) not null"`
-	IsDeleted bool      `xorm:"bool not null"`
-	Hash      string    `xorm:"char(32) not null"`
-	Manager   string    `xorm:"varchar(30) not null default ''"`
-	CreatedAt time.Time `xorm:"created not null"`
+	ID              string    `xorm:"char(36) pk"`
+	Name            string    `xorm:"text not null"`
+	Mime            string    `xorm:"text not null"`
+	Size            int64     `xorm:"bigint not null"`
+	CreatorID       string    `xorm:"char(36) not null"`
+	IsDeleted       bool      `xorm:"bool not null"`
+	Hash            string    `xorm:"char(32) not null"`
+	Manager         string    `xorm:"varchar(30) not null default ''"`
+	HasThumbnail    bool      `xorm:"bool not null"`
+	ThumbnailWidth  int       `xorm:"int not null"`
+	ThumbnailHeight int       `xorm:"int not null"`
+	CreatedAt       time.Time `xorm:"created not null"`
 }
 
 // TableName dbのtableの名前を返します
@@ -63,18 +91,51 @@ func (f *File) Create(src io.Reader) error {
 
 	writer, ok := fileManagers[f.Manager]
 	if !ok {
-		return fmt.Errorf("unknown file manager: %s", f.Manager)
+		return ErrFileUnknownManager
 	}
 
-	if err := writer.WriteByID(src, f.ID, f.Name, f.Mime); err != nil {
-		return fmt.Errorf("Failed to write data into file: %v", err)
-	}
+	eg, ctx := errgroup.WithContext(context.Background())
 
-	hash, err := calcMD5(src)
-	if err != nil {
+	fileSrc, fileWriter := io.Pipe()
+	thumbSrc, thumbWriter := io.Pipe()
+	hash := md5.New()
+
+	go func() {
+		defer fileWriter.Close()
+		defer thumbWriter.Close()
+		io.Copy(io.MultiWriter(fileWriter, hash, thumbWriter), src) // 並列化してるけど、pipeじゃなくてbuffer使わないとreaderがブロックしてて意味無い疑惑
+	}()
+
+	// fileの保存
+	eg.Go(func() error {
+		defer fileSrc.Close()
+		if err := writer.WriteByID(fileSrc, f.ID, f.Name, f.Mime); err != nil {
+			return fmt.Errorf("Failed to write data into file: %v", err)
+		}
+		return nil
+	})
+
+	// サムネイルの生成
+	eg.Go(func() error {
+		// アップロードされたファイルの拡張子が間違えてたり、変なの送ってきた場合
+		// サムネイルを生成しないだけで全体のエラーにはしない
+		defer thumbSrc.Close()
+		if err := GenerateThumbnail(ctx, f, thumbSrc); err != nil {
+			switch err {
+			case ErrFileThumbUnsupported:
+				return nil
+			default:
+				log.Error(err)
+			}
+		}
+		return nil
+	})
+
+	if err := eg.Wait(); err != nil {
 		return err
 	}
-	f.Hash = hash
+
+	f.Hash = hex.EncodeToString(hash.Sum(nil))
 
 	if _, err := db.Insert(f); err != nil {
 		return fmt.Errorf("Failed to create file")
@@ -109,7 +170,7 @@ func (f *File) Delete() error {
 func (f *File) Open() (io.ReadCloser, error) {
 	reader, ok := fileManagers[f.Manager]
 	if !ok {
-		return nil, fmt.Errorf("unknown file manager: %s", f.Manager)
+		return nil, ErrFileUnknownManager
 	}
 
 	return reader.OpenFileByID(f.ID)
@@ -124,6 +185,74 @@ func (f *File) GetRedirectURL() string {
 	return m.GetRedirectURL(f.ID)
 }
 
+// GenerateThumbnail サムネイル画像を生成します
+func GenerateThumbnail(ctx context.Context, f *File, src io.Reader) error {
+	var (
+		img       image.Image
+		err       error
+		thumbSize image.Point
+		dst       draw.Image
+		b         = &bytes.Buffer{}
+	)
+
+	writer, ok := fileManagers[f.Manager]
+	if !ok {
+		return ErrFileUnknownManager
+	}
+
+	switch f.Mime {
+	case "image/png":
+		img, err = png.Decode(src)
+	case "image/gif":
+		img, err = gif.Decode(src)
+	case "image/jpeg":
+		img, err = jpeg.Decode(src)
+	case "image/bmp":
+		img, err = bmp.Decode(src)
+	case "image/webp":
+		img, err = webp.Decode(src)
+	default: // Unsupported Type
+		return ErrFileThumbUnsupported
+	}
+	if err != nil {
+		return err
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		thumbSize = calcThumbnailSize(img.Bounds().Size())
+		dst = image.NewRGBA(image.Rectangle{Min: image.ZP, Max: thumbSize})
+		draw.Draw(dst, dst.Bounds(), image.White, image.ZP, draw.Src)
+		draw.ApproxBiLinear.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Src, nil)
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		if err := jpeg.Encode(b, dst, &jpeg.Options{Quality: 100}); err != nil {
+			return err
+		}
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		if err := writer.WriteByID(b, f.ID+"-thumb", f.ID+"-thumb.jpg", "image/jpeg"); err != nil {
+			return err
+		}
+	}
+
+	f.HasThumbnail = true
+	f.ThumbnailWidth = thumbSize.X
+	f.ThumbnailHeight = thumbSize.Y
+
+	return nil
+}
+
 // OpenFileByID ファイルを取得します
 func OpenFileByID(ID string) (io.ReadCloser, error) {
 	meta, err := GetMetaFileDataByID(ID)
@@ -133,7 +262,7 @@ func OpenFileByID(ID string) (io.ReadCloser, error) {
 
 	reader, ok := fileManagers[meta.Manager]
 	if !ok {
-		return nil, fmt.Errorf("unknown file manager: %s", meta.Manager)
+		return nil, ErrFileUnknownManager
 	}
 
 	return reader.OpenFileByID(ID)
@@ -154,13 +283,19 @@ func GetMetaFileDataByID(FileID string) (*File, error) {
 	return f, nil
 }
 
-// 与えられたデータに対してMD5によるハッシュ値を計算します
-func calcMD5(src io.Reader) (string, error) {
-	h := md5.New()
-	if _, err := io.Copy(h, src); err != nil {
-		return "", fmt.Errorf("Failed to calc md5")
+// サムネイルのサイズを計算します
+func calcThumbnailSize(size image.Point) image.Point {
+	if size.X <= thumbnailMaxWidth && size.Y <= thumbnailMaxHeight {
+		// 元画像がサムネイル画像より小さい
+		return size
 	}
-	return hex.EncodeToString(h.Sum(nil)), nil
+
+	ratio := float64(size.X) / float64(size.Y)
+
+	if ratio > thumbnailRatio {
+		return image.Pt(thumbnailMaxWidth, int(thumbnailMaxWidth/ratio))
+	}
+	return image.Pt(int(thumbnailMaxHeight*ratio), thumbnailMaxHeight)
 }
 
 // SetFileManager ファイルマネージャーリストにマネージャーをセットします
