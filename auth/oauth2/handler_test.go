fc6580f61399c091d23a20a57bbfb3767b084b19
takashi <30363887+wtks@users.noreply.github.com>
2018-03-28 02:32:40 +0900

    add TokenEndpointHandler Failure Test

--- auth/oauth2/handler_test.go
+++ auth/oauth2/handler_test.go
@@ -1,1 +1,64 @@
 package oauth2
+
+import (
+	"encoding/json"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+func TestHandler_TokenEndpointHandler_Failure1(t *testing.T) {
+	t.Parallel()
+
+	assert := assert.New(t)
+	e := echo.New()
+	h := &Handler{Store: NewStoreMock()}
+
+	f := url.Values{}
+	f.Set("grant_type", "ああああ")
+
+	req := httptest.NewRequest(echo.POST, "/", strings.NewReader(f.Encode()))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(h.TokenEndpointHandler(c)) {
+		assert.Equal(http.StatusBadRequest, rec.Code)
+		assert.Equal("no-store", rec.Header().Get("Cache-Control"))
+		assert.Equal("no-cache", rec.Header().Get("Pragma"))
+
+		res := errorResponse{}
+		if assert.NoError(json.NewDecoder(rec.Body).Decode(&res)) {
+			assert.Equal(errUnsupportedGrantType, res.ErrorType)
+		}
+	}
+}
+
+func TestHandler_TokenEndpointHandler_Failure2(t *testing.T) {
+	t.Parallel()
+
+	assert := assert.New(t)
+	e := echo.New()
+	h := &Handler{Store: NewStoreMock()}
+
+	req := httptest.NewRequest(echo.POST, "/", nil)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(h.TokenEndpointHandler(c)) {
+		assert.Equal(http.StatusBadRequest, rec.Code)
+		assert.Equal("no-store", rec.Header().Get("Cache-Control"))
+		assert.Equal("no-cache", rec.Header().Get("Pragma"))
+
+		res := errorResponse{}
+		if assert.NoError(json.NewDecoder(rec.Body).Decode(&res)) {
+			assert.Equal(errInvalidRequest, res.ErrorType)
+		}
+	}
+}
