03135347c5057adcb30e7bef07eeeedd60969952
takashi <30363887+wtks@users.noreply.github.com>
2018-07-01 18:29:39 +0900

    WIP #310 model/pins.go

--- model/pins.go
+++ model/pins.go
@@ -1,82 +1,63 @@
 package model
 
 import (
-	"fmt"
-	"github.com/traPtitech/traQ/utils/validator"
+	"github.com/jinzhu/gorm"
+	"github.com/satori/go.uuid"
 	"time"
 )
 
-//Pin ピン留めのレコード
+// Pin ピン留めのレコード
 type Pin struct {
-	ID        string    `xorm:"char(36) pk"       validate:"uuid,required"`
-	ChannelID string    `xorm:"char(36) not null" validate:"uuid,required"`
-	MessageID string    `xorm:"char(36) not null unique" validate:"uuid,required"`
-	UserID    string    `xorm:"char(36) not null" validate:"uuid,required"`
-	CreatedAt time.Time `xorm:"created not null"`
+	ID        string    `gorm:"type:char(36);primary_key"`
+	MessageID string    `gorm:"type:char(36);unique"`
+	Message   Message   `gorm:"association_autoupdate:false;association_autocreate:false"`
+	UserID    string    `gorm:"type:char(36)"`
+	CreatedAt time.Time `gorm:"precision:6"`
 }
 
-//TableName ピン留めテーブル名
+// TableName ピン留めテーブル名
 func (pin *Pin) TableName() string {
 	return "pins"
 }
 
-// Validate 構造体を検証します
-func (pin *Pin) Validate() error {
-	return validator.ValidateStruct(pin)
+// CreatePin ピン留めレコードを追加する
+func CreatePin(messageID, userID uuid.UUID) (uuid.UUID, error) {
+	id := uuid.NewV4()
+	p := &Pin{
+		ID:        id.String(),
+		MessageID: messageID.String(),
+		UserID:    userID.String(),
+	}
+	if err := db.Create(p).Error; err != nil {
+		return uuid.Nil, err
+	}
+	return id, nil
 }
 
-//Create ピン留めレコードを追加する
-func (pin *Pin) Create() (err error) {
-	pin.ID = CreateUUID()
-	if err = pin.Validate(); err != nil {
-		return
-	}
-
-	_, err = db.InsertOne(pin)
-	return
-}
-
-// Exists pinが存在するかどうかを判定する
-func (pin *Pin) Exists() (bool, error) {
-	if pin.ID == "" {
-		return false, fmt.Errorf("pin ID is empty")
-	}
-	return db.Get(pin)
-
-}
-
-//GetPin IDからピン留めを取得する
-func GetPin(ID string) (*Pin, error) {
-	if ID == "" {
-		return nil, fmt.Errorf("ID is empty")
-	}
-
-	pin := &Pin{}
-	if has, err := db.ID(ID).Get(pin); err != nil {
+// GetPin IDからピン留めを取得する
+func GetPin(id uuid.UUID) (p *Pin, err error) {
+	p = &Pin{}
+	err = db.Preload("Message").Where(Pin{ID: id.String()}).Take(p).Error
+	if err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, nil
+		}
 		return nil, err
-	} else if !has {
-		return nil, nil
 	}
-
-	return pin, nil
-}
-
-//GetPinsByChannelID あるチャンネルのピン留めを全部取得する
-func GetPinsByChannelID(channelID string) (pins []*Pin, err error) {
-	if channelID == "" {
-		return nil, fmt.Errorf("ChannelID is empty")
-	}
-
-	err = db.Where("channel_id = ?", channelID).Find(&pins)
 	return
 }
 
-//Delete ピン留めレコードを削除する
-func (pin *Pin) Delete() (err error) {
-	if pin.ID == "" {
-		return fmt.Errorf("ID is empty")
-	}
+// DeletePin ピン留めレコードを削除する
+func DeletePin(id uuid.UUID) error {
+	return db.Delete(Pin{ID: id.String()}).Error
+}
 
-	_, err = db.Delete(pin)
+// GetPinsByChannelID あるチャンネルのピン留めを全部取得する
+func GetPinsByChannelID(channelID uuid.UUID) (pins []*Pin, err error) {
+	err = db.
+		Joins("INNER JOIN messages ON messages.id = pins.message_id").
+		Where(Message{ChannelID: channelID.String()}).
+		Preload("Message").
+		Find(&pins).Error
 	return
 }
