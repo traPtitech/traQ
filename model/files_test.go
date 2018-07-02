f8179efaa103e090f560b5df0017502440076294
takashi <30363887+wtks@users.noreply.github.com>
2018-07-02 16:54:55 +0900

    WIP #310 model/files.go

--- model/files_test.go
+++ model/files_test.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"bytes"
+	"github.com/satori/go.uuid"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,47 +29,29 @@ func TestFile_Create(t *testing.T) {
 	assert.NoError(file.Create(writeData))
 }
 
-func TestFile_Exists(t *testing.T) {
-	assert, _, user, _ := beforeTest(t)
-
-	f := mustMakeFile(t, user.ID)
-	r := &File{ID: f.ID}
-
-	ok, err := r.Exists()
-	if assert.NoError(err) {
-		assert.True(ok)
-	}
-
-	r2 := &File{ID: CreateUUID()}
-
-	ok, err = r2.Exists()
-	if assert.NoError(err) {
-		assert.False(ok)
-	}
-}
-
-func TestFile_Delete(t *testing.T) {
+func TestDeleteFile(t *testing.T) {
 	assert, _, user, _ := beforeTest(t)
 
 	file := mustMakeFile(t, user.ID)
-	file.IsDeleted = true
 
-	assert.NoError(file.Delete())
+	assert.NoError(DeleteFile(file.GetID()))
+	_, err := fileManagers[""].OpenFileByID(file.ID)
+	assert.Error(err)
 }
 
-func TestGetFileByID(t *testing.T) {
+func TestOpenFileByID(t *testing.T) {
 	assert, _, user, _ := beforeTest(t)
 
 	f := mustMakeFile(t, user.ID)
-	file, err := OpenFileByID(f.ID)
-	assert.NoError(err)
-	defer file.Close()
-
-	buf := make([]byte, 512)
-	n, err := file.Read(buf)
-
+	file, err := OpenFileByID(f.GetID())
 	if assert.NoError(err) {
-		assert.Equal("test message", string(buf[:n]))
+		defer file.Close()
+
+		buf := make([]byte, 512)
+		n, err := file.Read(buf)
+		if assert.NoError(err) {
+			assert.Equal("test message", string(buf[:n]))
+		}
 	}
 }
 
@@ -76,12 +59,13 @@ func TestGetMetaFileDataByID(t *testing.T) {
 	assert, _, user, _ := beforeTest(t)
 
 	file := mustMakeFile(t, user.ID)
-	result, err := GetMetaFileDataByID(file.ID)
+	result, err := GetMetaFileDataByID(file.GetID())
 	if assert.NoError(err) {
 		assert.Equal(file.ID, result.ID)
 	}
 
-	none, err := GetMetaFileDataByID("wrongID")
-	assert.NoError(err)
-	assert.Nil(none)
+	none, err := GetMetaFileDataByID(uuid.Nil)
+	if assert.NoError(err) {
+		assert.Nil(none)
+	}
 }
