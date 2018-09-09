30a26aeb53173b88fe35187308a84eb45e3df345
takashi <30363887+wtks@users.noreply.github.com>
2018-09-09 10:24:17 +0900

    fix lint error

--- utils/icon.go
+++ utils/icon.go
@@ -9,6 +9,7 @@ const iconSize = 256
 
 var iconSettings = qidenticon.DefaultSettings()
 
+// GenerateIcon アイコン画像を生成します
 func GenerateIcon(salt string) image.Image {
 	return qidenticon.Render(qidenticon.Code(salt), iconSize, iconSettings)
 }
