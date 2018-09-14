d21a634dbb2e8ed42893010776f29e92cbd83820
takashi <30363887+wtks@users.noreply.github.com>
2018-09-14 21:26:56 +0900

    fix the critical issue of trying to unlock unlocked mutexes

--- utils/key_locker.go
+++ utils/key_locker.go
@@ -1,6 +1,9 @@
 package utils
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // KeyLocker KeyによるMutex
 type KeyLocker struct {
@@ -8,22 +11,35 @@ type KeyLocker struct {
 	pool  *sync.Pool
 }
 
+type refCounter struct {
+	counter int64
+	lock    *sync.Mutex
+}
+
 // Lock キーをロックします
 func (l *KeyLocker) Lock(key interface{}) {
-	l.getLocker(key).Lock()
+	m := l.getLocker(key)
+	atomic.AddInt64(&m.counter, 1)
+	m.lock.Lock()
 }
 
 // Unlock キーをアンロックします
 func (l *KeyLocker) Unlock(key interface{}) {
 	m := l.getLocker(key)
-	m.Unlock()
-	l.pool.Put(m)
-	l.inUse.Delete(key)
+	m.lock.Unlock()
+	atomic.AddInt64(&m.counter, -1)
+	if m.counter <= 0 {
+		l.pool.Put(m)
+		l.inUse.Delete(key)
+	}
 }
 
-func (l *KeyLocker) getLocker(key interface{}) *sync.Mutex {
-	res, _ := l.inUse.LoadOrStore(key, l.pool.Get().(*sync.Mutex))
-	return res.(*sync.Mutex)
+func (l *KeyLocker) getLocker(key interface{}) *refCounter {
+	res, _ := l.inUse.LoadOrStore(key, &refCounter{
+		counter: 0,
+		lock:    l.pool.Get().(*sync.Mutex),
+	})
+	return res.(*refCounter)
 }
 
 // NewKeyLocker KeyLockerを生成します
