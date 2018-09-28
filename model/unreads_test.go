043711b730867aa5c78729ce6e2886f5aaaf223a
takashi <30363887+wtks@users.noreply.github.com>
2018-09-28 14:35:28 +0900

    make unreads' tests parallel

--- model/unreads_test.go
+++ model/unreads_test.go
@@ -2,7 +2,7 @@ package model
 
 import (
 	"github.com/satori/go.uuid"
-	"strconv"
+	"github.com/traPtitech/traQ/utils"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,65 +13,82 @@ func TestUnreadTableName(t *testing.T) {
 	assert.Equal(t, "unreads", (&Unread{}).TableName())
 }
 
-func TestSetMessageUnread(t *testing.T) {
-	assert, _, user, channel := beforeTest(t)
-	testMessage := mustMakeMessage(t, user.GetUID(), channel.ID)
+// TestParallelGroup9 並列テストグループ9 競合がないようなサブテストにすること
+func TestParallelGroup9(t *testing.T) {
+	assert, _, _, _ := beforeTest(t)
 
-	assert.NoError(SetMessageUnread(user.GetUID(), testMessage.GetID()))
-}
+	// SetMessageUnread
+	t.Run("TestSetMessageUnread", func(t *testing.T) {
+		t.Parallel()
 
-func TestGetUnreadMessagesByUserID(t *testing.T) {
-	assert, _, user, channel := beforeTest(t)
+		user := mustMakeUser(t, utils.RandAlphabetAndNumberString(20))
+		channel := mustMakeChannelDetail(t, user.GetUID(), utils.RandAlphabetAndNumberString(20), "")
+		testMessage := mustMakeMessage(t, user.GetUID(), channel.ID)
 
-	for i := 0; i < 10; i++ {
-		mustMakeMessageUnread(t, user.GetUID(), mustMakeMessage(t, user.GetUID(), channel.ID).GetID())
-	}
+		assert.NoError(SetMessageUnread(user.GetUID(), testMessage.GetID()))
+	})
 
-	if unreads, err := GetUnreadMessagesByUserID(user.GetUID()); assert.NoError(err) {
-		assert.Len(unreads, 10)
-	}
-	if unreads, err := GetUnreadMessagesByUserID(uuid.Nil); assert.NoError(err) {
-		assert.Len(unreads, 0)
-	}
-}
+	// GetUnreadMessagesByUserID
+	t.Run("TestGetUnreadMessagesByUserID", func(t *testing.T) {
+		t.Parallel()
 
-func TestDeleteUnreadsByMessageID(t *testing.T) {
-	assert, _, user, channel := beforeTest(t)
+		user := mustMakeUser(t, utils.RandAlphabetAndNumberString(20))
+		channel := mustMakeChannelDetail(t, user.GetUID(), utils.RandAlphabetAndNumberString(20), "")
+		for i := 0; i < 10; i++ {
+			mustMakeMessageUnread(t, user.GetUID(), mustMakeMessage(t, user.GetUID(), channel.ID).GetID())
+		}
 
-	testMessage := mustMakeMessage(t, user.GetUID(), channel.ID)
-	testMessage2 := mustMakeMessage(t, user.GetUID(), channel.ID)
-	for i := 0; i < 10; i++ {
-		mustMakeMessageUnread(t, mustMakeUser(t, "test"+strconv.Itoa(i*2)).GetUID(), testMessage.GetID())
-		mustMakeMessageUnread(t, mustMakeUser(t, "test"+strconv.Itoa(i*2+1)).GetUID(), testMessage2.GetID())
-	}
+		if unreads, err := GetUnreadMessagesByUserID(user.GetUID()); assert.NoError(err) {
+			assert.Len(unreads, 10)
+		}
+		if unreads, err := GetUnreadMessagesByUserID(uuid.Nil); assert.NoError(err) {
+			assert.Len(unreads, 0)
+		}
+	})
 
-	if assert.NoError(DeleteUnreadsByMessageID(testMessage.GetID())) {
-		count := 0
-		db.Model(Unread{}).Count(&count)
-		assert.Equal(10, count)
-	}
-	if assert.NoError(DeleteUnreadsByMessageID(testMessage2.GetID())) {
-		count := 0
-		db.Model(Unread{}).Count(&count)
-		assert.Equal(0, count)
-	}
-}
+	// DeleteUnreadsByMessageID
+	t.Run("TestDeleteUnreadsByMessageID", func(t *testing.T) {
+		t.Parallel()
 
-func TestDeleteUnreadsByChannelID(t *testing.T) {
-	assert, _, user, channel := beforeTest(t)
+		user := mustMakeUser(t, utils.RandAlphabetAndNumberString(20))
+		channel := mustMakeChannelDetail(t, user.GetUID(), utils.RandAlphabetAndNumberString(20), "")
+		testMessage := mustMakeMessage(t, user.GetUID(), channel.ID)
+		testMessage2 := mustMakeMessage(t, user.GetUID(), channel.ID)
+		for i := 0; i < 10; i++ {
+			mustMakeMessageUnread(t, mustMakeUser(t, utils.RandAlphabetAndNumberString(20)).GetUID(), testMessage.GetID())
+			mustMakeMessageUnread(t, mustMakeUser(t, utils.RandAlphabetAndNumberString(20)).GetUID(), testMessage2.GetID())
+		}
 
-	creator := mustMakeUser(t, "creator")
+		if assert.NoError(DeleteUnreadsByMessageID(testMessage.GetID())) {
+			count := -1
+			db.Model(Unread{}).Where(&Unread{MessageID: testMessage.ID}).Count(&count)
+			assert.Equal(0, count)
+		}
+		if assert.NoError(DeleteUnreadsByMessageID(testMessage2.GetID())) {
+			count := -1
+			db.Model(Unread{}).Where(&Unread{MessageID: testMessage2.ID}).Count(&count)
+			assert.Equal(0, count)
+		}
+	})
 
-	testMessage := mustMakeMessage(t, creator.GetUID(), channel.ID)
-	mustMakeMessageUnread(t, user.GetUID(), testMessage.GetID())
+	// DeleteUnreadsByChannelID
+	t.Run("TestDeleteUnreadsByChannelID", func(t *testing.T) {
+		t.Parallel()
 
-	testChannel := mustMakeChannel(t, creator.GetUID(), "-unreads")
-	testMessage2 := mustMakeMessage(t, creator.GetUID(), testChannel.ID)
-	mustMakeMessageUnread(t, user.GetUID(), testMessage2.GetID())
+		creator := mustMakeUser(t, utils.RandAlphabetAndNumberString(20))
+		user := mustMakeUser(t, utils.RandAlphabetAndNumberString(20))
+		channel := mustMakeChannelDetail(t, creator.GetUID(), utils.RandAlphabetAndNumberString(20), "")
+		channel2 := mustMakeChannelDetail(t, creator.GetUID(), utils.RandAlphabetAndNumberString(20), "")
+		testMessage := mustMakeMessage(t, creator.GetUID(), channel.ID)
+		testMessage2 := mustMakeMessage(t, creator.GetUID(), channel2.ID)
 
-	if assert.NoError(DeleteUnreadsByChannelID(channel.ID, user.GetUID())) {
-		count := 0
-		db.Model(Unread{}).Count(&count)
-		assert.Equal(1, count)
-	}
+		mustMakeMessageUnread(t, user.GetUID(), testMessage.GetID())
+		mustMakeMessageUnread(t, user.GetUID(), testMessage2.GetID())
+
+		if assert.NoError(DeleteUnreadsByChannelID(channel.ID, user.GetUID())) {
+			count := 0
+			db.Model(Unread{}).Where(&Unread{UserID: user.ID}).Count(&count)
+			assert.Equal(1, count)
+		}
+	})
 }
