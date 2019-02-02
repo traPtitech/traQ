734cee594b797c9a7374b03967985f712627798c
takashi <30363887+wtks@users.noreply.github.com>
2019-02-02 20:36:05 +0900

    workaround for #403

---
+++ router/activity.go
@@ -0,0 +1,50 @@
+package router
+
+import (
+	"github.com/labstack/echo"
+	"github.com/traPtitech/traQ/model"
+	"net/http"
+)
+
+// GET /activity/latest-messages
+func GetActivityLatestMessages(c echo.Context) error {
+	userID := getRequestUserID(c)
+
+	req := struct {
+		Limit         int  `query:"limit"  validate:"min=1,max=50"`
+		SubscribeOnly bool `query:"subscribe"`
+	}{
+		Limit:         50,
+		SubscribeOnly: true,
+	}
+	if err := bindAndValidate(c, &req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	messages, err := model.GetChannelLatestMessagesByUserID(userID, req.Limit, req.SubscribeOnly)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	reports, err := model.GetMessageReportsByReporterID(userID)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+	hidden := make(map[string]bool)
+	for _, v := range reports {
+		hidden[v.MessageID] = true
+	}
+
+	res := make([]*MessageForResponse, 0, len(messages))
+	for _, message := range messages {
+		ms := formatMessage(message)
+		if hidden[message.ID] {
+			ms.Reported = true
+		}
+		res = append(res, ms)
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
