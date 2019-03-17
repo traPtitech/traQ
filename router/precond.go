141498b3b6b288bf3d4994e2c022563c2d961822
takashi <30363887+wtks@users.noreply.github.com>
2019-03-18 02:30:53 +0900

    add tests

--- router/precond.go
+++ router/precond.go
@@ -19,12 +19,8 @@ func scanETag(s string) (etag string, remain string) {
 	}
 	for i := start + 1; i < len(s); i++ {
 		c := s[i]
-		switch {
-		case c == 0x21 || c >= 0x23 && c <= 0x7E || c >= 0x80:
-		case c == '"':
+		if c == '"' {
 			return s[:i+1], s[i+1:]
-		default:
-			return "", ""
 		}
 	}
 	return "", ""
