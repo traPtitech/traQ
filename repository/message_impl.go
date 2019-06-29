eedd9764e10706c862d4d0a7d46d03ac57e67d0d
motoki317 <motoki317@gmail.com>
2019-06-30 01:17:02 +0900

    Add force-notification channels to api /activity/latest-messages response when subscribe: true

--- repository/message_impl.go
+++ repository/message_impl.go
@@ -281,13 +281,12 @@ func (repo *GormRepository) GetChannelLatestMessagesByUserID(userID uuid.UUID, l
 		query = `
 SELECT m.id, m.user_id, m.channel_id, m.text, m.created_at, m.updated_at, m.deleted_at
 FROM channel_latest_messages clm
-         INNER JOIN users_subscribe_channels s ON clm.channel_id = s.channel_id
          INNER JOIN messages m ON clm.message_id = m.id
          INNER JOIN channels c ON clm.channel_id = c.id
-WHERE s.user_id = 'USER_ID'
-  AND c.deleted_at IS NULL
+WHERE c.deleted_at IS NULL
   AND c.is_public = TRUE
   AND m.deleted_at IS NULL
+  AND (c.is_forced = TRUE OR c.id IN (SELECT s.channel_id FROM users_subscribe_channels s WHERE s.user_id = 'USER_ID'))
 ORDER BY clm.date_time DESC
 `
 		query = strings.Replace(query, "USER_ID", userID.String(), -1)
