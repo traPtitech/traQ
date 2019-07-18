b8932a0e36ecd6ff2245ce26bb1ab09e9a9b7eb6
Yudai FUJIWARA <ptr.yudai@gmail.com>
2019-07-18 17:51:17 +0900

    Implemented #355

--- sessions/config.go
+++ sessions/config.go
@@ -2,9 +2,10 @@ package sessions
 
 const (
 	// CookieName セッションクッキー名
-	CookieName    = "r_session"
-	tableName     = "r_sessions"
-	cacheSize     = 4096
-	mutexSize     = 1024
-	sessionMaxAge = 60 * 60 * 24 * 14 // 2 weeks
+	CookieName     = "r_session"
+	tableName      = "r_sessions"
+	cacheSize      = 4096
+	mutexSize      = 1024
+	sessionMaxAge  = 60 * 60 * 24 * 14 // 2 weeks
+	sessionKeepAge = 60 * 60 * 24 * 14 // 2 weeks
 )
