ce18b5b97b8f6a05e4273cb4d3ceab54f3e7d6d6
takashi <t.k.mk3.110@gmail.com>
2019-11-11 00:06:43 +0900

    close #445

--- router/users.go
+++ router/users.go
@@ -188,6 +188,7 @@ func (h *Handlers) PutUserPassword(c echo.Context) error {
 			return internalServerError(err, h.requestContextLogger(c))
 		}
 	}
+	sessions.DestroyByUserID(userID)
 
 	return c.NoContent(http.StatusNoContent)
 }
@@ -260,6 +261,7 @@ func (h *Handlers) PutPassword(c echo.Context) error {
 			return internalServerError(err, h.requestContextLogger(c))
 		}
 	}
+	sessions.DestroyByUserID(user.ID)
 
 	return c.NoContent(http.StatusNoContent)
 }
