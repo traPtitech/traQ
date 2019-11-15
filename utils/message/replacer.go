aadcebb8fd27e7693ff70bbb429f78e622aa1c6c
sapphi-red <green@sapphi.red>
2019-11-15 15:39:42 +0900

    katex内でもメンションが置換されていた
    refs traPtitech/traQ-UI#1171

--- utils/message/replacer.go
+++ utils/message/replacer.go
@@ -2,14 +2,20 @@ package message
 
 import (
 	"fmt"
-	"github.com/gofrs/uuid"
 	"regexp"
 	"strings"
+
+	"github.com/gofrs/uuid"
 )
 
 var (
 	mentionRegex = regexp.MustCompile(`[@＠]([\S]+)`)
 	channelRegex = regexp.MustCompile(`[#＃]([a-zA-Z0-9_/-]+)`)
+)
+
+const (
+	backQuoteRune = rune('`')
+	dollarRune    = rune('$')
 )
 
 // ReplaceMapper メッセージ埋め込み置換マッピング
@@ -35,24 +41,74 @@ func NewReplacer(mapper ReplaceMapper) *Replacer {
 // Replace 埋め込みを置換します
 func (re *Replacer) Replace(m string) string {
 	inCodeBlock := false
+	inLatexBlock := false
 	lines := strings.Split(m, "\n")
 	for i, line := range lines {
-		if strings.HasPrefix(line, "```") {
+		if !inLatexBlock && strings.HasPrefix(line, "```") {
 			inCodeBlock = !inCodeBlock
 		}
-		if !inCodeBlock {
-			inQuote := false
-			split := strings.Split(line, "`")
-			for j, s := range split {
-				if !inQuote {
-					split[j] = re.replaceChannel(re.replaceMention(s))
-				}
-				inQuote = !inQuote
-			}
-			lines[i] = strings.Join(split, "`")
+		if !inCodeBlock && strings.HasPrefix(line, "$$") {
+			inLatexBlock = !inLatexBlock
 		}
+		if inCodeBlock || inLatexBlock {
+			continue
+		}
+		// 「```」のブロックでも「$$」ブロック内でもないときに置換
+
+		chs := []rune(line)
+		newChs := make([]rune, 0, len(chs))
+		// 「`」「$」で囲まれていないところの始めの文字のindex
+		noExpressionStartIndex := 0
+		for i := 0; i < len(chs); i++ {
+			ch := chs[i]
+			if ch != backQuoteRune && ch != dollarRune {
+				continue
+			}
+
+			// 囲まれていない場所が終了したのでその箇所は置換する
+			newChs = append(newChs, []rune(
+				re.replaceAll(
+					string(chs[noExpressionStartIndex:i]),
+				),
+			)...)
+
+			if ch == dollarRune {
+				// 「`」は「$」よりも優先されるので
+				// 「$ ` $」のように「`」がペアの「$」より前にあるときは
+				// 「$」のペアとして処理しない
+				backQuoteI := strings.IndexRune(string(chs[i+1:]), backQuoteRune)
+				dollarI := strings.IndexRune(string(chs[i+1:]), dollarRune)
+				if backQuoteI != -1 && dollarI != -1 && backQuoteI < dollarI {
+					newChs = append(newChs, ch)
+					noExpressionStartIndex = i + 1
+					continue
+				}
+			}
+			newI := strings.IndexRune(string(chs[i+1:]), ch)
+			if newI == -1 {
+				// 「$」/「`」のペアがないとき
+				newChs = append(newChs, ch)
+				noExpressionStartIndex = i + 1
+				continue
+			}
+			newI += i + 1
+			newChs = append(newChs, chs[i:newI]...)
+			i = newI
+			noExpressionStartIndex = newI
+		}
+		// 最後のペア以降の置換
+		newChs = append(newChs, []rune(
+			re.replaceAll(
+				string(chs[noExpressionStartIndex:]),
+			),
+		)...)
+		lines[i] = string(newChs)
 	}
 	return strings.Join(lines, "\n")
+}
+
+func (re *Replacer) replaceAll(m string) string {
+	return re.replaceMention(re.replaceChannel(m))
 }
 
 func (re *Replacer) replaceMention(m string) string {
