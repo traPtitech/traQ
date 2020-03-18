7ab3439dfa350a731088ab6a8b55c5497a7edbc9
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-03-18 21:08:59 +0900

    fix stamp column

--- model/stamp_palettes.go
+++ model/stamp_palettes.go
@@ -1,18 +1,54 @@
 package model
 
 import (
+	"database/sql/driver"
 	"github.com/gofrs/uuid"
 	"time"
+	"strings"
+	"errors"
 )
+
+type UUIDs []uuid.UUID
+
+func (arr UUIDs) Value() (driver.Value, error) {
+	idStr := []string{}
+	for _, id := range arr {
+		idStr = append(idStr, id.String())
+	}
+	return strings.Join(idStr, ","), nil
+}
+
+func (arr *UUIDs) Scan(src interface{}) error {
+	switch s := src.(type) {
+	case nil:
+		*arr = UUIDs{}
+	case string:
+		idSlice := strings.Split(s, ",")
+		for _, id := range idSlice {
+			stampID, _ := uuid.FromString(id)
+			*arr = append(*arr, stampID)
+		}
+	case []byte:
+		str := string(s)
+		idSlice := strings.Split(str, ",")
+		for _, id := range idSlice {
+			stampID, _ := uuid.FromString(id)
+			*arr = append(*arr, stampID)
+		}
+	default:
+		return errors.New("failed to scan Stamps")
+	}
+	return nil
+}
 
 type StampPalette struct {
 	ID          uuid.UUID `gorm:"type:char(36);not null;primary_key"`
 	Name        string    `gorm:"type:varchar(30);not null"`
-	Description string    `gorm:"type:varchar(1000)"`
-	Stamps      []Stamp
+	Description string    `gorm:"type:text(1000);not null"`
+	Stamps      UUIDs	`gorm:"type:text;not null"`
 	CreatorID   uuid.UUID  `gorm:"type:char(36);not null"`
 	CreatedAt   time.Time  `gorm:"precision:6"`
-	UpdatedAt   *time.Time `gorm:"precision:6"`
+	UpdatedAt   time.Time `gorm:"precision:6"`
 }
 
 // TableName StampPalettes構造体のテーブル名
