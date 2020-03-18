44384c48329a9d2ed1bffaf28ff6a8622fc1519c
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-03-18 21:49:06 +0900

    fix name

--- model/clip.go
+++ model/clip.go
@@ -7,7 +7,7 @@ import (
 )
 
 // ClipFolders クリップフォルダーの構造体
-type ClipFolders struct {
+type ClipFolder struct {
 	ID          uuid.UUID `gorm:"type:char(36);not null;primary_key"`
 	Name        string    `gorm:"type:varchar(30);not null"`
 	Description string    `gorm:"type:text"`
@@ -16,18 +16,18 @@ type ClipFolders struct {
 }
 
 // TableName ClipFolders構造体のテーブル名
-func (*ClipFolders) TableName() string {
+func (*ClipFolder) TableName() string {
 	return "clip_folders"
 }
 
 // ClipFolderMessages クリップフォルダーのメッセージの構造体
-type ClipFolderMessages struct {
+type ClipFolderMessage struct {
 	FolderID  uuid.UUID `gorm:"type:char(36);not null;primary_key"`
 	MessageID uuid.UUID `gorm:"type:char(36);not null;primary_key"`
 	ClippedAt time.Time `gorm:"precision:6"`
 }
 
 // TableName ClipFolderMessages構造体のテーブル名
-func (*ClipFolderMessages) TableName() string {
+func (*ClipFolderMessage) TableName() string {
 	return "clip_folder_messages"
 }
