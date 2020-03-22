463f3d94fd5ec51b24a1a9d4772cad2624930be5
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-03-22 16:10:29 +0900

    clipのrouterの実装

--- router/v3/clips.go
+++ router/v3/clips.go
@@ -3,38 +3,80 @@ package v3
 import (
 	"net/http"
 
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/traQ/repository"
+	"github.com/traPtitech/traQ/router/consts"
+	"github.com/traPtitech/traQ/router/extension/herror"
 )
+
+type PostClipFolderRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
 
 // PostClipFolders POST /clip-folders
 func (h *Handlers) CreateClipFolders(c echo.Context) error {
 	userID := getRequestUserID(c)
 
-	var req PostChannelRequest
+	var req PostClipFolderRequest
 	if err := bindAndValidate(c, &req); err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK, h.Realtime.OnlineCounter.GetOnlineUserIDs())
+
+	cf, err := h.Repo.CreateClipFolder(userID, req.Name, req.Description)
+	if err != nil {
+		return herror.InternalServerError(err)
+	}
+
+	return c.JSON(http.StatusCreated, formatClipFolder(cf))
+
 }
 
 // GetClipFolders GET /clip-folders
 func (h *Handlers) GetClipFolders(c echo.Context) error {
-	return c.JSON(http.StatusOK, h.Realtime.OnlineCounter.GetOnlineUserIDs())
+	userID := getRequestUserID(c)
+
+	cfs, err := h.Repo.GetClipFoldersByUserID(userID)
+	if err != nil {
+		return herror.InternalServerError(err)
+	}
+
+	return c.JSON(http.StatusOK, formatClipFolders(cfs))
 }
 
 // GetClipFolder GET /clip-folders/:folderID
 func (h *Handlers) GetClipFolder(c echo.Context) error {
-	return c.JSON(http.StatusOK, h.Realtime.OnlineCounter.GetOnlineUserIDs())
+	return c.JSON(http.StatusOK, formatClipFolder(getParamClipFolder(c)))
 }
 
 // DeleteClipFolder DELETE /clip-folder/:folderID
 func (h *Handlers) DeleteClipFolder(c echo.Context) error {
-	return c.JSON(http.StatusOK, h.Realtime.OnlineCounter.GetOnlineUserIDs())
+	folderID := getParamAsUUID(c, consts.ParamClipFolderID)
+
+	if err := h.Repo.DeleteClipFolder(folderID); err != nil {
+		return err
+	}
+	return c.NoContent(http.StatusNoContent)
 }
 
 // EditClipFolder PATCH /clip-folders/:folderID
 func (h *Handlers) EditClipFolder(c echo.Context) error {
-	return c.JSON(http.StatusOK, h.Realtime.OnlineCounter.GetOnlineUserIDs())
+	cf := getParamClipFolder(c)
+
+	var req PostClipFolderRequest
+	if err := bindAndValidate(c, &req); err != nil {
+		return err
+	}
+
+	if err := h.Repo.UpdateClipFolder(cf.ID, req.Name, req.Description); err != nil {
+		return herror.InternalServerError(err)
+	}
+	return c.NoContent(http.StatusNoContent)
+}
+
+type PostClipFolderMessageRequest struct {
+	MessageID uuid.UUID
 }
 
 // PostClipFolderMessage POST /clip-folders/:folderID/messages
@@ -56,10 +98,31 @@ func (h *Handlers) PostClipFolderMessages(c echo.Context) error {
 
 // GetFolderMessages GET /clip-folders/:folderID/messages
 func (h *Handlers) GetClipFolderMessages(c echo.Context) error {
-	return c.JSON(http.StatusOK, h.Realtime.OnlineCounter.GetOnlineUserIDs())
+	cf := getParamClipFolder(c)
+
+	var req repository.ClipFolderMessageQuery
+	if err := bindAndValidate(c, &req); err != nil {
+		return err
+	}
+
+	// wip:moreを読み捨ててる
+	messages, _, err := h.Repo.GetClipFolderMessages(cf.ID, req)
+	if err != nil {
+		return herror.InternalServerError(err)
+	}
+	// wip
+	// c.Response().Header().Set(consts.HeaderMore, strconv.FormatBool(more))
+
+	return c.JSON(http.StatusOK, formatClipFolderMessages(cf.ID, messages))
 }
 
 // DeleteFolderMessages DELETE /clip-folders/:folderID/messages/:messageID
 func (h *Handlers) DeleteClipFolderMessages(c echo.Context) error {
-	return c.JSON(http.StatusOK, h.Realtime.OnlineCounter.GetOnlineUserIDs())
+	m := getParamMessage(c)
+	cf := getParamClipFolder(c)
+	if err := h.Repo.DeleteClipFolderMessage(cf.ID, m.ID); err != nil {
+		return herror.InternalServerError(err)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
