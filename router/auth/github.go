f752c54970eaa8d9a0b6616e67fc4a2f5924e839
takashi <t.k.mk3.110@gmail.com>
2020-03-30 02:22:49 +0900

    refactoring

--- router/auth/github.go
+++ router/auth/github.go
@@ -3,14 +3,9 @@ package auth
 import (
 	"context"
 	"fmt"
-	"github.com/gofrs/uuid"
 	json "github.com/json-iterator/go"
 	"github.com/labstack/echo/v4"
-	"github.com/traPtitech/traQ/model"
-	"github.com/traPtitech/traQ/rbac/role"
 	"github.com/traPtitech/traQ/repository"
-	"github.com/traPtitech/traQ/router/extension/herror"
-	"github.com/traPtitech/traQ/router/sessions"
 	"go.uber.org/zap"
 	"golang.org/x/exp/utf8string"
 	"golang.org/x/oauth2"
@@ -118,95 +113,7 @@ func (p *GithubProvider) LoginHandler(c echo.Context) error {
 }
 
 func (p *GithubProvider) CallbackHandler(c echo.Context) error {
-	code := c.QueryParam("code")
-	state := c.QueryParam("state")
-	if len(code) == 0 || len(state) == 0 {
-		return herror.BadRequest("missing code or state")
-	}
-
-	cookie, err := c.Cookie(cookieName)
-	if err != nil {
-		return herror.BadRequest("missing cookie")
-	}
-	if cookie.Value != state {
-		return herror.BadRequest("invalid state")
-	}
-
-	t, err := p.oa2.Exchange(context.Background(), code)
-	if err != nil {
-		return herror.BadRequest("token exchange failed")
-	}
-
-	tu, err := p.FetchUserInfo(t)
-	if err != nil {
-		return herror.InternalServerError(err)
-	}
-
-	if !tu.IsLoginAllowedUser() {
-		return c.String(http.StatusForbidden, "You are not permitted to access traQ")
-	}
-
-	user, err := p.repo.GetUserByExternalID(tu.GetProviderName(), tu.GetID(), false)
-	if err != nil {
-		if err != repository.ErrNotFound {
-			return herror.InternalServerError(err)
-		}
-
-		if !p.config.RegisterUserIfNotFound {
-			return herror.Unauthorized("You are not a member of traQ")
-		}
-
-		args := repository.CreateUserArgs{
-			Name:        tu.GetName(),
-			DisplayName: tu.GetDisplayName(),
-			Role:        role.User,
-			ExternalLogin: &model.ExternalProviderUser{
-				ProviderName: tu.GetProviderName(),
-				ExternalID:   tu.GetID(),
-			},
-		}
-
-		if b, err := tu.GetProfileImage(); err == nil && b != nil {
-			fid, err := processProfileIcon(p.repo, b)
-			if err == nil {
-				args.IconFileID = uuid.NullUUID{Valid: true, UUID: fid}
-			}
-		}
-
-		user, err = p.repo.CreateUser(args)
-		if err != nil {
-			if err == repository.ErrAlreadyExists {
-				return herror.Conflict("name conflicts") // TODO 名前被りをどうするか
-			}
-			return herror.InternalServerError(err)
-		}
-		p.logger.Info("New user was created by external auth (github)",
-			zap.Stringer("id", user.GetID()),
-			zap.String("name", user.GetName()),
-			zap.String("githubId", tu.GetID()),
-			zap.String("githubLogin", tu.GetName()))
-	}
-
-	// ユーザーのアカウント状態の確認
-	if !user.IsActive() {
-		return herror.Forbidden("this account is currently suspended")
-	}
-
-	sess, err := sessions.Get(c.Response(), c.Request(), true)
-	if err != nil {
-		return herror.InternalServerError(err)
-	}
-
-	if err := sess.SetUser(user.GetID()); err != nil {
-		return herror.InternalServerError(err)
-	}
-	p.logger.Info("User was logged in by external auth (github)",
-		zap.Stringer("id", user.GetID()),
-		zap.String("name", user.GetName()),
-		zap.String("githubId", tu.GetID()),
-		zap.String("githubLogin", tu.GetName()))
-
-	return c.Redirect(http.StatusFound, "/")
+	return defaultCallbackHandler(p, &p.oa2, p.repo, p.config.RegisterUserIfNotFound)(c)
 }
 
 func (p *GithubProvider) FetchUserInfo(t *oauth2.Token) (UserInfo, error) {
@@ -240,4 +147,8 @@ func (p *GithubProvider) FetchUserInfo(t *oauth2.Token) (UserInfo, error) {
 	ui.profileImageURL = u.Picture
 
 	return &ui, nil
+}
+
+func (p *GithubProvider) L() *zap.Logger {
+	return p.logger
 }
--- router/auth/provider.go
+++ router/auth/provider.go
@@ -2,13 +2,18 @@ package auth
 
 import (
 	"bytes"
+	"context"
 	"github.com/disintegration/imaging"
 	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/traQ/model"
+	"github.com/traPtitech/traQ/rbac/role"
 	"github.com/traPtitech/traQ/repository"
 	"github.com/traPtitech/traQ/router/consts"
+	"github.com/traPtitech/traQ/router/extension/herror"
+	"github.com/traPtitech/traQ/router/sessions"
 	"github.com/traPtitech/traQ/utils"
+	"go.uber.org/zap"
 	"golang.org/x/oauth2"
 	"net/http"
 	"time"
@@ -23,6 +28,7 @@ type Provider interface {
 	FetchUserInfo(t *oauth2.Token) (UserInfo, error)
 	LoginHandler(c echo.Context) error
 	CallbackHandler(c echo.Context) error
+	L() *zap.Logger
 }
 
 type UserInfo interface {
@@ -46,6 +52,102 @@ func defaultLoginHandler(oac *oauth2.Config) echo.HandlerFunc {
 			HttpOnly: true,
 		})
 		return c.Redirect(http.StatusFound, oac.AuthCodeURL(state))
+	}
+}
+
+func defaultCallbackHandler(p Provider, oac *oauth2.Config, repo repository.Repository, allowSignUp bool) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		code := c.QueryParam("code")
+		state := c.QueryParam("state")
+		if len(code) == 0 || len(state) == 0 {
+			return herror.BadRequest("missing code or state")
+		}
+
+		cookie, err := c.Cookie(cookieName)
+		if err != nil {
+			return herror.BadRequest("missing cookie")
+		}
+		if cookie.Value != state {
+			return herror.BadRequest("invalid state")
+		}
+
+		t, err := oac.Exchange(context.Background(), code)
+		if err != nil {
+			return herror.BadRequest("token exchange failed")
+		}
+
+		tu, err := p.FetchUserInfo(t)
+		if err != nil {
+			return herror.InternalServerError(err)
+		}
+
+		if !tu.IsLoginAllowedUser() {
+			return c.String(http.StatusForbidden, "You are not permitted to access traQ")
+		}
+
+		user, err := repo.GetUserByExternalID(tu.GetProviderName(), tu.GetID(), false)
+		if err != nil {
+			if err != repository.ErrNotFound {
+				return herror.InternalServerError(err)
+			}
+
+			if !allowSignUp {
+				return herror.Unauthorized("You are not a member of traQ")
+			}
+
+			args := repository.CreateUserArgs{
+				Name:        tu.GetName(),
+				DisplayName: tu.GetDisplayName(),
+				Role:        role.User,
+				ExternalLogin: &model.ExternalProviderUser{
+					ProviderName: tu.GetProviderName(),
+					ExternalID:   tu.GetID(),
+				},
+			}
+
+			if b, err := tu.GetProfileImage(); err == nil && b != nil {
+				fid, err := processProfileIcon(repo, b)
+				if err == nil {
+					args.IconFileID = uuid.NullUUID{Valid: true, UUID: fid}
+				}
+			}
+
+			user, err = repo.CreateUser(args)
+			if err != nil {
+				if err == repository.ErrAlreadyExists {
+					return herror.Conflict("name conflicts") // TODO 名前被りをどうするか
+				}
+				return herror.InternalServerError(err)
+			}
+			p.L().Info("New user was created by external auth",
+				zap.Stringer("id", user.GetID()),
+				zap.String("name", user.GetName()),
+				zap.String("providerName", tu.GetProviderName()),
+				zap.String("externalId", tu.GetID()),
+				zap.String("externalName", tu.GetName()))
+		}
+
+		// ユーザーのアカウント状態の確認
+		if !user.IsActive() {
+			return herror.Forbidden("this account is currently suspended")
+		}
+
+		sess, err := sessions.Get(c.Response(), c.Request(), true)
+		if err != nil {
+			return herror.InternalServerError(err)
+		}
+
+		if err := sess.SetUser(user.GetID()); err != nil {
+			return herror.InternalServerError(err)
+		}
+		p.L().Info("User was logged in by external auth",
+			zap.Stringer("id", user.GetID()),
+			zap.String("name", user.GetName()),
+			zap.String("providerName", tu.GetProviderName()),
+			zap.String("externalId", tu.GetID()),
+			zap.String("externalName", tu.GetName()))
+
+		return c.Redirect(http.StatusFound, "/")
 	}
 }
 
