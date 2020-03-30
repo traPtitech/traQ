40f788988c5764745cca2ed2d18bd7696fd55e5d
takashi <t.k.mk3.110@gmail.com>
2020-03-30 14:03:45 +0900

    fix json response

--- model/stamp_palettes.go
+++ model/stamp_palettes.go
@@ -48,13 +48,13 @@ func (arr *UUIDs) Scan(src interface{}) error {
 }
 
 type StampPalette struct {
-	ID          uuid.UUID `gorm:"type:char(36);not null;primary_key"`
-	Name        string    `gorm:"type:varchar(30);not null"`
-	Description string    `gorm:"type:text;not null"`
-	Stamps      UUIDs     `gorm:"type:text;not null"`
-	CreatorID   uuid.UUID `gorm:"type:char(36);not null;index"`
-	CreatedAt   time.Time `gorm:"precision:6"`
-	UpdatedAt   time.Time `gorm:"precision:6"`
+	ID          uuid.UUID `gorm:"type:char(36);not null;primary_key" json:"id"`
+	Name        string    `gorm:"type:varchar(30);not null" json:"name"`
+	Description string    `gorm:"type:text;not null" json:"description"`
+	Stamps      UUIDs     `gorm:"type:text;not null" json:"stamps"`
+	CreatorID   uuid.UUID `gorm:"type:char(36);not null;index" json:"creatorId"`
+	CreatedAt   time.Time `gorm:"precision:6" json:"createdAt"`
+	UpdatedAt   time.Time `gorm:"precision:6" json:"updatedAt"`
 }
 
 // TableName StampPalettes構造体のテーブル名
