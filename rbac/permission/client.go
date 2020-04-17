8a99382b0e13d7a08b14832895116a6d0834b9cb
takashi <t.k.mk3.110@gmail.com>
2020-04-17 14:15:33 +0900

    add UserPermission Enum

--- rbac/permission/client.go
+++ rbac/permission/client.go
@@ -5,17 +5,17 @@ import (
 )
 
 const (
-	// GetMyTokens : 自トークン情報取得権限
+	// GetMyTokens 自トークン情報取得権限
 	GetMyTokens = rbac.Permission("get_my_tokens")
-	// RevokeMyToken : 自トークン削除権限
+	// RevokeMyToken 自トークン削除権限
 	RevokeMyToken = rbac.Permission("revoke_my_token")
-	// GetClients : クライアント情報取得権限
+	// GetClients クライアント情報取得権限
 	GetClients = rbac.Permission("get_clients")
-	// CreateClient : 新規クライアント登録権限
+	// CreateClient 新規クライアント登録権限
 	CreateClient = rbac.Permission("create_client")
-	// EditMyClient : クライアント情報編集権限
+	// EditMyClient クライアント情報編集権限
 	EditMyClient = rbac.Permission("edit_my_client")
-	// DeleteMyClient : クライアント削除権限
+	// DeleteMyClient クライアント削除権限
 	DeleteMyClient = rbac.Permission("delete_my_client")
 	// ManageOthersClient 他人のClientの管理権限
 	ManageOthersClient = rbac.Permission("manage_others_client")
