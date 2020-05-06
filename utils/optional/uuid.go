21715d170672923bbe82a9cf3bf4b38faa85f1d7
takashi <t.k.mk3.110@gmail.com>
2020-05-07 00:32:07 +0900

    optional.UUID implements TextMarshaler, TextUnmarshaler interface

--- utils/optional/uuid.go
+++ utils/optional/uuid.go
@@ -14,3 +14,23 @@ func UUIDFrom(v uuid.UUID) UUID {
 func NewUUID(v uuid.UUID, valid bool) UUID {
 	return UUID{NullUUID: uuid.NullUUID{UUID: v, Valid: valid}}
 }
+
+func (u *UUID) UnmarshalText(text []byte) error {
+	str := string(text)
+	if str == "" || str == "null" {
+		u.Valid = false
+		return nil
+	}
+	if err := u.UUID.UnmarshalText(text); err != nil {
+		return err
+	}
+	u.Valid = true
+	return nil
+}
+
+func (u UUID) MarshalText() ([]byte, error) {
+	if u.Valid {
+		return u.UUID.MarshalText()
+	}
+	return []byte{}, nil
+}
