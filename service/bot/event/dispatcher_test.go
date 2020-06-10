05b4168dd7e7b2fe38d9fb0361eede8545c822d2
takashi <t.k.mk3.110@gmail.com>
2020-06-10 13:30:15 +0900

    add tests

---
+++ service/bot/event/dispatcher_test.go
@@ -0,0 +1,82 @@
+package event
+
+import (
+	"github.com/gofrs/uuid"
+	"github.com/golang/mock/gomock"
+	"github.com/traPtitech/traQ/model"
+	"github.com/traPtitech/traQ/service/bot/event/mock_event"
+	"github.com/traPtitech/traQ/service/bot/event/payload"
+	"testing"
+)
+
+func TestUnicast(t *testing.T) {
+	t.Parallel()
+
+	t.Run("no target", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		d := mock_event.NewMockDispatcher(ctrl)
+
+		Unicast(d, Ping, payload.MakePing(), nil)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		d := mock_event.NewMockDispatcher(ctrl)
+
+		b1 := &model.Bot{ID: uuid.NewV3(uuid.Nil, "b1")}
+		p := payload.MakePing()
+		body, release, _ := makePayloadJSON(p)
+		defer release()
+
+		d.EXPECT().
+			Send(b1, Ping, body).
+			Return(true).
+			Times(1)
+
+		Unicast(d, Ping, p, b1)
+	})
+
+}
+
+func TestMulticast(t *testing.T) {
+	t.Parallel()
+
+	t.Run("no target", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		d := mock_event.NewMockDispatcher(ctrl)
+
+		Multicast(d, Ping, payload.MakePing(), []*model.Bot{})
+	})
+
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		d := mock_event.NewMockDispatcher(ctrl)
+
+		b1 := &model.Bot{ID: uuid.NewV3(uuid.Nil, "b1")}
+		b2 := &model.Bot{ID: uuid.NewV3(uuid.Nil, "b2")}
+		b3 := &model.Bot{ID: uuid.NewV3(uuid.Nil, "b3")}
+		bots := []*model.Bot{b1, b2, b1, b3}
+		p := payload.MakePing()
+		body, release, _ := makePayloadJSON(p)
+		defer release()
+
+		d.EXPECT().
+			Send(b1, Ping, body).
+			Return(true).
+			Times(1)
+		d.EXPECT().
+			Send(b2, Ping, body).
+			Return(true).
+			Times(1)
+		d.EXPECT().
+			Send(b3, Ping, body).
+			Return(true).
+			Times(1)
+
+		Multicast(d, Ping, p, bots)
+	})
+}
