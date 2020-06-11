c2df003813c180919fe8e49c111848e478d22d5d
FourmiSushi <neko@fourmisushi.soy>
2020-06-11 21:44:12 +0900

    add spoiler_test.go

---
+++ utils/message/spoiler_test.go
@@ -0,0 +1,36 @@
+package message
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFillSpoiler(t *testing.T) {
+	t.Parallel()
+
+	type Case struct {
+		Message string
+		Filled  string
+	}
+
+	cases := []Case{
+		{Message: "!!Mark!!", Filled: "****"},
+		{Message: "x !!!!foo!! bar!!", Filled: "x *******"},
+		{Message: "x !!foo !!bar!!!!", Filled: "x *******"},
+		{Message: "x !!!!foo!!!!", Filled: "x ***"},
+		{Message: "x !!!foo!!!", Filled: "x ****!"},
+		{Message: "!!foo !!bar!! baz!!", Filled: "***********"},
+		{Message: "foo !! bar !! baz", Filled: "foo !! bar !! baz"},
+		{Message: "x !!a !!foo!!!!!!!!!!!bar!! b!!", Filled: "x *****!!******"},
+		{Message: "x !!a !!foo!!!!!!!!!!!!bar!! b!!", Filled: "x *****!!!!*****"},
+	}
+
+	for _, v := range cases {
+		t.Run(v.Filled, func(t *testing.T) {
+			t.Parallel()
+			filled := FillSpoiler(v.Message)
+			assert.EqualValues(t, v.Filled, filled)
+		})
+	}
+}
