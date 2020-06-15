0625b7f5c11143fa1ee4b3671863138085ee5899
takashi <t.k.mk3.110@gmail.com>
2020-06-16 02:19:12 +0900

    add tests

--- service/file/manager_impl_test.go
+++ service/file/manager_impl_test.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"bytes"
 	"errors"
 	"github.com/gofrs/uuid"
 	"github.com/golang/mock/gomock"
@@ -9,9 +10,15 @@ import (
 	"github.com/traPtitech/traQ/repository"
 	"github.com/traPtitech/traQ/repository/mock_repository"
 	"github.com/traPtitech/traQ/service/imaging"
+	"github.com/traPtitech/traQ/service/imaging/mock_imaging"
+	imaging2 "github.com/traPtitech/traQ/utils/imaging"
+	"github.com/traPtitech/traQ/utils/optional"
 	"github.com/traPtitech/traQ/utils/storage"
 	"github.com/traPtitech/traQ/utils/storage/mock_storage"
 	"go.uber.org/zap"
+	"image/png"
+	"io"
+	"io/ioutil"
 	"testing"
 	"time"
 )
@@ -25,6 +32,244 @@ func initFM(t *testing.T, repo repository.FileRepository, fs storage.FileStorage
 		ip:   ip,
 		l:    zap.NewNop(),
 	}
+}
+
+func TestManagerImpl_Save(t *testing.T) {
+	t.Parallel()
+
+	t.Run("text file", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		repo := mock_repository.NewMockFileRepository(ctrl)
+		fs := mock_storage.NewMockFileStorage(ctrl)
+		fm := initFM(t, repo, fs, nil)
+
+		data := []byte("test text file")
+		hash := "7e6d5d7ae4965bfecc6d818f76eb832b"
+		args := SaveArgs{
+			FileName:  "test.txt",
+			FileSize:  int64(len(data)),
+			MimeType:  "text/plain",
+			FileType:  model.FileTypeUserFile,
+			ChannelID: optional.UUIDFrom(uuid.NewV3(uuid.Nil, "c")),
+			Src:       bytes.NewReader(data),
+		}
+
+		fs.EXPECT().
+			SaveByKey(gomock.Any(), gomock.Any(), args.FileName, args.MimeType, args.FileType).
+			DoAndReturn(func(src io.Reader, key, name, contentType string, fileType model.FileType) error {
+				_, _ = io.Copy(ioutil.Discard, src)
+				return nil
+			}).
+			Times(1)
+		repo.EXPECT().
+			SaveFileMeta(gomock.Any(), []*model.FileACLEntry{{UserID: optional.UUIDFrom(uuid.Nil), Allow: optional.BoolFrom(true)}}).
+			DoAndReturn(func(meta *model.FileMeta, acl []*model.FileACLEntry) error {
+				meta.CreatedAt = time.Now()
+				return nil
+			}).
+			Times(1)
+
+		result, err := fm.Save(args)
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, result.GetID())
+			assert.EqualValues(t, args.FileName, result.GetFileName())
+			assert.EqualValues(t, args.FileSize, result.GetFileSize())
+			assert.EqualValues(t, args.MimeType, result.GetMIMEType())
+			assert.EqualValues(t, args.FileType, result.GetFileType())
+			assert.EqualValues(t, args.ChannelID, result.GetUploadChannelID())
+			assert.EqualValues(t, args.CreatorID, result.GetCreatorID())
+			assert.EqualValues(t, hash, result.GetMD5Hash())
+			assert.NotEmpty(t, result.GetCreatedAt())
+			assert.False(t, result.HasThumbnail())
+		}
+	})
+
+	t.Run("file with thumbnail", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		repo := mock_repository.NewMockFileRepository(ctrl)
+		fs := mock_storage.NewMockFileStorage(ctrl)
+		fm := initFM(t, repo, fs, nil)
+
+		data := []byte("test text file")
+		hash := "7e6d5d7ae4965bfecc6d818f76eb832b"
+		thumb := imaging2.GenerateIcon("test")
+		args := SaveArgs{
+			FileName:  "dummy.png",
+			FileSize:  int64(len(data)),
+			MimeType:  "image/png",
+			FileType:  model.FileTypeUserFile,
+			ChannelID: optional.UUIDFrom(uuid.NewV3(uuid.Nil, "c")),
+			Src:       bytes.NewReader(data),
+			Thumbnail: thumb,
+		}
+
+		fs.EXPECT().
+			SaveByKey(gomock.Any(), gomock.Any(), args.FileName, args.MimeType, args.FileType).
+			DoAndReturn(func(src io.Reader, key, name, contentType string, fileType model.FileType) error {
+				_, _ = io.Copy(ioutil.Discard, src)
+				return nil
+			}).
+			Times(1)
+		fs.EXPECT().
+			SaveByKey(gomock.Any(), gomock.Any(), gomock.Any(), "image/png", model.FileTypeThumbnail).
+			DoAndReturn(func(src io.Reader, key, name, contentType string, fileType model.FileType) error {
+				_, err := png.Decode(src)
+				return err
+			}).
+			Times(1)
+		repo.EXPECT().
+			SaveFileMeta(gomock.Any(), []*model.FileACLEntry{{UserID: optional.UUIDFrom(uuid.Nil), Allow: optional.BoolFrom(true)}}).
+			DoAndReturn(func(meta *model.FileMeta, acl []*model.FileACLEntry) error {
+				meta.CreatedAt = time.Now()
+				return nil
+			}).
+			Times(1)
+
+		result, err := fm.Save(args)
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, result.GetID())
+			assert.EqualValues(t, args.FileName, result.GetFileName())
+			assert.EqualValues(t, args.FileSize, result.GetFileSize())
+			assert.EqualValues(t, args.MimeType, result.GetMIMEType())
+			assert.EqualValues(t, args.FileType, result.GetFileType())
+			assert.EqualValues(t, args.ChannelID, result.GetUploadChannelID())
+			assert.EqualValues(t, args.CreatorID, result.GetCreatorID())
+			assert.EqualValues(t, hash, result.GetMD5Hash())
+			assert.NotEmpty(t, result.GetCreatedAt())
+			assert.True(t, result.HasThumbnail())
+			assert.EqualValues(t, "image/png", result.GetThumbnailMIMEType())
+			assert.EqualValues(t, thumb.Bounds().Size().X, result.GetThumbnailWidth())
+			assert.EqualValues(t, thumb.Bounds().Size().Y, result.GetThumbnailHeight())
+		}
+	})
+
+	t.Run("image with generating thumbnail (io.ReadSeeker)", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		repo := mock_repository.NewMockFileRepository(ctrl)
+		fs := mock_storage.NewMockFileStorage(ctrl)
+		ip := mock_imaging.NewMockProcessor(ctrl)
+		fm := initFM(t, repo, fs, ip)
+
+		data := []byte("test text file")
+		hash := "7e6d5d7ae4965bfecc6d818f76eb832b"
+		thumb := imaging2.GenerateIcon("test")
+		args := SaveArgs{
+			FileName:  "dummy.png",
+			FileSize:  int64(len(data)),
+			MimeType:  "image/png",
+			FileType:  model.FileTypeUserFile,
+			ChannelID: optional.UUIDFrom(uuid.NewV3(uuid.Nil, "c")),
+			Src:       bytes.NewReader(data),
+		}
+
+		fs.EXPECT().
+			SaveByKey(gomock.Any(), gomock.Any(), args.FileName, args.MimeType, args.FileType).
+			Do(func(src io.Reader, key, name, contentType string, fileType model.FileType) {
+				_, _ = io.Copy(ioutil.Discard, src)
+			}).
+			Return(nil).
+			Times(1)
+		fs.EXPECT().
+			SaveByKey(gomock.Any(), gomock.Any(), gomock.Any(), "image/png", model.FileTypeThumbnail).
+			DoAndReturn(func(src io.Reader, key, name, contentType string, fileType model.FileType) error {
+				_, err := png.Decode(src)
+				return err
+			}).
+			Times(1)
+		repo.EXPECT().
+			SaveFileMeta(gomock.Any(), []*model.FileACLEntry{{UserID: optional.UUIDFrom(uuid.Nil), Allow: optional.BoolFrom(true)}}).
+			Do(func(meta *model.FileMeta, acl []*model.FileACLEntry) { meta.CreatedAt = time.Now() }).
+			Return(nil).
+			Times(1)
+		ip.EXPECT().
+			Thumbnail(gomock.Any()).
+			Do(func(src io.ReadSeeker) { _, _ = io.Copy(ioutil.Discard, src) }).
+			Return(thumb, nil).
+			Times(1)
+
+		result, err := fm.Save(args)
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, result.GetID())
+			assert.EqualValues(t, args.FileName, result.GetFileName())
+			assert.EqualValues(t, args.FileSize, result.GetFileSize())
+			assert.EqualValues(t, args.MimeType, result.GetMIMEType())
+			assert.EqualValues(t, args.FileType, result.GetFileType())
+			assert.EqualValues(t, args.ChannelID, result.GetUploadChannelID())
+			assert.EqualValues(t, args.CreatorID, result.GetCreatorID())
+			assert.EqualValues(t, hash, result.GetMD5Hash())
+			assert.NotEmpty(t, result.GetCreatedAt())
+			assert.True(t, result.HasThumbnail())
+			assert.EqualValues(t, "image/png", result.GetThumbnailMIMEType())
+			assert.EqualValues(t, thumb.Bounds().Size().X, result.GetThumbnailWidth())
+			assert.EqualValues(t, thumb.Bounds().Size().Y, result.GetThumbnailHeight())
+		}
+	})
+
+	t.Run("image with generating thumbnail (io.Reader)", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		repo := mock_repository.NewMockFileRepository(ctrl)
+		fs := mock_storage.NewMockFileStorage(ctrl)
+		ip := mock_imaging.NewMockProcessor(ctrl)
+		fm := initFM(t, repo, fs, ip)
+
+		data := []byte("test text file")
+		hash := "7e6d5d7ae4965bfecc6d818f76eb832b"
+		thumb := imaging2.GenerateIcon("test")
+		args := SaveArgs{
+			FileName:  "dummy.png",
+			FileSize:  int64(len(data)),
+			MimeType:  "image/png",
+			FileType:  model.FileTypeUserFile,
+			ChannelID: optional.UUIDFrom(uuid.NewV3(uuid.Nil, "c")),
+			Src:       bytes.NewBuffer(data),
+		}
+
+		fs.EXPECT().
+			SaveByKey(gomock.Any(), gomock.Any(), args.FileName, args.MimeType, args.FileType).
+			Do(func(src io.Reader, key, name, contentType string, fileType model.FileType) {
+				_, _ = io.Copy(ioutil.Discard, src)
+			}).
+			Return(nil).
+			Times(1)
+		fs.EXPECT().
+			SaveByKey(gomock.Any(), gomock.Any(), gomock.Any(), "image/png", model.FileTypeThumbnail).
+			DoAndReturn(func(src io.Reader, key, name, contentType string, fileType model.FileType) error {
+				_, err := png.Decode(src)
+				return err
+			}).
+			Times(1)
+		repo.EXPECT().
+			SaveFileMeta(gomock.Any(), []*model.FileACLEntry{{UserID: optional.UUIDFrom(uuid.Nil), Allow: optional.BoolFrom(true)}}).
+			Do(func(meta *model.FileMeta, acl []*model.FileACLEntry) { meta.CreatedAt = time.Now() }).
+			Return(nil).
+			Times(1)
+		ip.EXPECT().
+			Thumbnail(gomock.Any()).
+			Do(func(src io.ReadSeeker) { _, _ = io.Copy(ioutil.Discard, src) }).
+			Return(thumb, nil).
+			Times(1)
+
+		result, err := fm.Save(args)
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, result.GetID())
+			assert.EqualValues(t, args.FileName, result.GetFileName())
+			assert.EqualValues(t, args.FileSize, result.GetFileSize())
+			assert.EqualValues(t, args.MimeType, result.GetMIMEType())
+			assert.EqualValues(t, args.FileType, result.GetFileType())
+			assert.EqualValues(t, args.ChannelID, result.GetUploadChannelID())
+			assert.EqualValues(t, args.CreatorID, result.GetCreatorID())
+			assert.EqualValues(t, hash, result.GetMD5Hash())
+			assert.NotEmpty(t, result.GetCreatedAt())
+			assert.True(t, result.HasThumbnail())
+			assert.EqualValues(t, "image/png", result.GetThumbnailMIMEType())
+			assert.EqualValues(t, thumb.Bounds().Size().X, result.GetThumbnailWidth())
+			assert.EqualValues(t, thumb.Bounds().Size().Y, result.GetThumbnailHeight())
+		}
+	})
 }
 
 func TestManagerImpl_Get(t *testing.T) {
