eb75c7cd27de6ce8fbcc2e767b8b75085732d5d0
takashi <t.k.mk3.110@gmail.com>
2020-06-16 02:30:21 +0900

    go mod tidy & fix lint

--- testutils/test_repository.go
+++ testutils/test_repository.go
@@ -16,9 +16,7 @@ import (
 	"github.com/traPtitech/traQ/repository"
 	"github.com/traPtitech/traQ/service/rbac/role"
 	"github.com/traPtitech/traQ/utils"
-	"github.com/traPtitech/traQ/utils/ioext"
 	"github.com/traPtitech/traQ/utils/set"
-	"github.com/traPtitech/traQ/utils/storage"
 	"github.com/traPtitech/traQ/utils/validator"
 )
 
@@ -1730,79 +1728,6 @@ func (repo *TestRepository) GetClipFolderMessages(uuid.UUID, repository.ClipFold
 
 func (repo *TestRepository) GetMessageClips(uuid.UUID, uuid.UUID) ([]*model.ClipFolderMessage, error) {
 	panic("implement me")
-}
-
-type fileMetaImpl struct {
-	meta *model.FileMeta
-	fs   storage.FileStorage
-}
-
-func (f *fileMetaImpl) GetID() uuid.UUID {
-	return f.meta.ID
-}
-
-func (f *fileMetaImpl) GetFileName() string {
-	return f.meta.Name
-}
-
-func (f *fileMetaImpl) GetMIMEType() string {
-	return f.meta.Mime
-}
-
-func (f *fileMetaImpl) GetFileSize() int64 {
-	return f.meta.Size
-}
-
-func (f *fileMetaImpl) GetFileType() model.FileType {
-	return f.meta.Type
-}
-
-func (f *fileMetaImpl) GetCreatorID() optional.UUID {
-	return f.meta.CreatorID
-}
-
-func (f *fileMetaImpl) GetMD5Hash() string {
-	return f.meta.Hash
-}
-
-func (f *fileMetaImpl) HasThumbnail() bool {
-	return f.meta.HasThumbnail
-}
-
-func (f *fileMetaImpl) GetThumbnailMIMEType() string {
-	return f.meta.ThumbnailMime.String
-}
-
-func (f *fileMetaImpl) GetThumbnailWidth() int {
-	return f.meta.ThumbnailWidth
-}
-
-func (f *fileMetaImpl) GetThumbnailHeight() int {
-	return f.meta.ThumbnailHeight
-}
-
-func (f *fileMetaImpl) GetUploadChannelID() optional.UUID {
-	return f.meta.ChannelID
-}
-
-func (f *fileMetaImpl) GetCreatedAt() time.Time {
-	return f.meta.CreatedAt
-}
-
-func (f *fileMetaImpl) Open() (ioext.ReadSeekCloser, error) {
-	return f.fs.OpenFileByKey(f.GetID().String(), f.GetFileType())
-}
-
-func (f *fileMetaImpl) OpenThumbnail() (ioext.ReadSeekCloser, error) {
-	if !f.HasThumbnail() {
-		return nil, repository.ErrNotFound
-	}
-	return f.fs.OpenFileByKey(f.GetID().String()+"-thumb", model.FileTypeThumbnail)
-}
-
-func (f *fileMetaImpl) GetAlternativeURL() string {
-	url, _ := f.fs.GenerateAccessURL(f.GetID().String(), f.GetFileType())
-	return url
 }
 
 func (repo *TestRepository) GetFileMetas(repository.FilesQuery) (result []*model.FileMeta, more bool, err error) {
