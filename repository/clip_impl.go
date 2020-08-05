b29fb8657e48eb02892c9384b6f07ef56eb68d33
motoki317 <motoki317@gmail.com>
2020-08-05 10:50:16 +0900

    fix gorm delete syntax in clip folder delete

--- repository/clip_impl.go
+++ repository/clip_impl.go
@@ -104,7 +104,10 @@ func (repo *GormRepository) DeleteClipFolder(folderID uuid.UUID) error {
 		if err := tx.First(&cf, &model.ClipFolder{ID: folderID}).Error; err != nil {
 			return convertError(err)
 		}
-		return tx.Delete(&model.ClipFolderMessage{FolderID: folderID}).Delete(&model.ClipFolder{ID: folderID}).Error
+		return tx.
+			Delete(&model.ClipFolderMessage{}, &model.ClipFolderMessage{FolderID: folderID}).
+			Delete(&model.ClipFolder{ID: folderID}).
+			Error
 	})
 	if err != nil {
 		return err
