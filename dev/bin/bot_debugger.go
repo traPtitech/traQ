33ac1436a2d66c53e468fc7d459dd1cdb699d09b
takashi <t.k.mk3.110@gmail.com>
2020-10-17 12:36:22 +0900

    add botdebugger

---
+++ dev/bin/bot_debugger.go
@@ -0,0 +1,29 @@
+// +build tools
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "p", 5555, "listen port")
+	flag.Parse()
+	http.HandleFunc("/", botHandler)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func botHandler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%s\n", dump)
+
+	w.WriteHeader(http.StatusNoContent)
+}
