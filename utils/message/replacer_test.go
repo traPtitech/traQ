f6b634fc8cfce5a1e39cfcc5f9cdd90eebee1bbb
sapphi-red <green@sapphi.red>
2021-01-18 23:39:08 +0900

    replacerが動作しないパターンの修正
    @のあとに@がスペースなしで出現する場合
    `@)、(@takashi_trap @takashi_trap)`

--- utils/message/replacer_test.go
+++ utils/message/replacer_test.go
@@ -106,6 +106,10 @@ func TestReplacer_Replace(t *testing.T) {
 			"@takashi_trapo @takashi_trape",
 			"!{\"type\":\"group\",\"raw\":\"@takashi_trapo\",\"id\":\"dfabf0c9-5de0-46ee-9721-2525e8bb3d46\"} !{\"type\":\"user\",\"raw\":\"@takashi_trape\",\"id\":\"dfdff0c9-5de0-46ee-9721-2525e8bb3d46\"}",
 		},
+		{
+			"@)、(@takashi_trap @takashi_trap)",
+			"@)、(!{\"type\":\"user\",\"raw\":\"@takashi_trap\",\"id\":\"dfdff0c9-5de0-46ee-9721-2525e8bb3d45\"} !{\"type\":\"user\",\"raw\":\"@takashi_trap\",\"id\":\"dfdff0c9-5de0-46ee-9721-2525e8bb3d45\"})",
+		},
 	}
 	for _, v := range tt {
 		assert.Equal(t, v[1], re.Replace(v[0]))
