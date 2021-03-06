3b71e5e3641e41a48e0d46244e0fa8b4453547ac
hijiki51 <hibiki0719euph@gmail.com>
2021-03-07 00:43:30 +0900

    add models

---
+++ model/user_settings.go
@@ -0,0 +1,19 @@
+package model
+
+import "github.com/gofrs/uuid"
+
+// UserSettings ユーザー設定の構造体
+type UserSettings struct {
+	UserID         uuid.UUID `gorm:"type:char(36);not null;primary_key"`
+	NotifyCitation bool      `gorm:"type:boolean"`
+}
+
+// TableName UserSettings構造体のテーブル名
+func (us *UserSettings) TableName() string {
+	return "user_settings"
+}
+
+// IsNotifyCitationEnabled メッセージの引用通知が有効かどうかを返します
+func (us *UserSettings) IsNotifyCitationEnabled() bool {
+	return us.NotifyCitation
+}
