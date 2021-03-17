8331519b4106bdcb5380285fcc1d4856766d84a6
hijiki51 <hibiki0719euph@gmail.com>
2021-03-18 01:36:22 +0900

    fix json tag

--- model/user_settings.go
+++ model/user_settings.go
@@ -4,7 +4,7 @@ import "github.com/gofrs/uuid"
 
 // UserSettings ユーザー設定の構造体
 type UserSettings struct {
-	UserID         uuid.UUID `gorm:"type:char(36);not null;primary_key"`
+	UserID         uuid.UUID `gorm:"type:char(36);not null;primary_key;" json:"id"`
 	NotifyCitation bool      `gorm:"type:boolean" json:"notifyCitation"`
 }
 
