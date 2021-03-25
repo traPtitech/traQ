5e3a54006e3b69a0ffb1345779f60bb786bd34a7
motoki317 <motoki317@gmail.com>
2021-03-25 10:47:08 +0900

    update es

--- service/search/es.go
+++ service/search/es.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	esRequiredVersion = "7.10.1"
+	esRequiredVersion = "7.10.2"
 	esIndexPrefix     = "traq_"
 	esMessageIndex    = "message"
 	esDateFormat      = "2006-01-02T15:04:05Z"
