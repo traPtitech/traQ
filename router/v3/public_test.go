d869634dd5bdd27271f0001ca7d78db7497861bb
motoki317 <motoki317@gmail.com>
2021-05-30 20:43:03 +0900

    💚 add public test

--- router/v3/public_test.go
+++ router/v3/public_test.go
@@ -3,18 +3,49 @@ package v3
 import (
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/traPtitech/traQ/repository"
+	file2 "github.com/traPtitech/traQ/service/file"
+	"github.com/traPtitech/traQ/service/rbac/role"
+	"github.com/traPtitech/traQ/utils/random"
 )
 
 func TestHandlers_GetVersion(t *testing.T) {
 	t.Parallel()
+
+	path := "/api/v3/version"
 	env := Setup(t, common1)
 
 	e := env.R(t)
-	obj := e.GET("/api/v3/version").
+	obj := e.GET(path).
 		Expect().
 		Status(http.StatusOK).
 		JSON().
 		Object()
 	obj.Value("version").String().Equal("version")
 	obj.Value("revision").String().Equal("revision")
+}
+
+func TestHandlers_GetPublicUserIcon(t *testing.T) {
+	t.Parallel()
+
+	path := "/api/v3/public/icon/{username}"
+	env := Setup(t, common1)
+	iconFileID, err := file2.GenerateIconFile(env.FM, "test")
+	require.NoError(t, err)
+	user, err := env.Repository.CreateUser(repository.CreateUserArgs{
+		Name:       random.AlphaNumeric(20),
+		Password:   "totallyASecurePassword",
+		Role:       role.User,
+		IconFileID: iconFileID,
+	})
+	require.NoError(t, err)
+
+	e := env.R(t)
+	e.GET(path, user.GetName()).
+		Expect().
+		Status(http.StatusOK).
+		ContentType("image/png")
 }
