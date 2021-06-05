aa5f2c2b066dd4b7bbef6fd2bfad290637f3bfdc
motoki317 <motoki317@gmail.com>
2021-06-05 18:58:07 +0900

    ♻️ rm user settings unreachable codes

--- router/v3/user_settings.go
+++ router/v3/user_settings.go
@@ -4,73 +4,55 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/traPtitech/traQ/repository"
+
 	"github.com/traPtitech/traQ/router/extension/herror"
 )
 
-// PutUserSettingsRequest PUT /user/me/settings/notify-citation リクエストボディ
-type PutUserSettingsRequest struct {
-	NotifyCitation bool `json:"notifyCitation"`
-}
-
-type getNotifyCitationResponce struct {
+// PutMyNotifyCitationRequest PUT /user/me/settings/notify-citation リクエストボディ
+type PutMyNotifyCitationRequest struct {
 	NotifyCitation bool `json:"notifyCitation"`
 }
 
 // PutMyNotifyCitation PUT /user/me/settings/notify-citation
 func (h *Handlers) PutMyNotifyCitation(c echo.Context) error {
 	id := getRequestUserID(c)
-	var us PutUserSettingsRequest
+
+	var us PutMyNotifyCitationRequest
 	if err := bindAndValidate(c, &us); err != nil {
 		return err
 	}
-	err := h.Repo.UpdateNotifyCitation(id, us.NotifyCitation)
 
-	if err != nil {
-		switch {
-		case err == repository.ErrNilID:
-			return herror.BadRequest("Invalid url")
-		case err == repository.ErrNotFound:
-			return herror.NotFound("User not found")
-		default:
-			return herror.InternalServerError(err)
-		}
+	if err := h.Repo.UpdateNotifyCitation(id, us.NotifyCitation); err != nil {
+		return herror.InternalServerError(err)
 	}
 
 	return c.NoContent(http.StatusNoContent)
-
 }
 
 // GetMySettings GET /user/me/settings
 func (h *Handlers) GetMySettings(c echo.Context) error {
 	id := getRequestUserID(c)
+
 	us, err := h.Repo.GetUserSettings(id)
 	if err != nil {
-		switch {
-		case err == repository.ErrNilID:
-			return herror.BadRequest("Invalid url")
-		case err == repository.ErrNotFound:
-			return herror.NotFound("User not found")
-		default:
-			return herror.InternalServerError(err)
-		}
+		return herror.InternalServerError(err)
 	}
+
 	return c.JSON(http.StatusOK, us)
 }
 
 // GetMyNotifyCitation GET /user/me/settings/notify-citation
 func (h *Handlers) GetMyNotifyCitation(c echo.Context) error {
 	id := getRequestUserID(c)
+
 	nc, err := h.Repo.GetNotifyCitation(id)
 	if err != nil {
-		switch {
-		case err == repository.ErrNilID:
-			return herror.BadRequest("Invalid url")
-		case err == repository.ErrNotFound:
-			return herror.NotFound("User not found")
-		default:
-			return herror.InternalServerError(err)
-		}
+		return herror.InternalServerError(err)
 	}
-	return c.JSON(http.StatusOK, &getNotifyCitationResponce{NotifyCitation: nc})
+
+	type res struct {
+		NotifyCitation bool `json:"notifyCitation"`
+	}
+
+	return c.JSON(http.StatusOK, &res{NotifyCitation: nc})
 }
