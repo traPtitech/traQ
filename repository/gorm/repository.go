1f5311059f82db2d66ae6ce931992a84491c53b4
motoki317 <motoki317@gmail.com>
2022-10-09 15:04:41 +0900

    Refactor stamp repository

--- repository/gorm/repository.go
+++ repository/gorm/repository.go
@@ -14,17 +14,17 @@ type Repository struct {
 	db     *gorm.DB
 	hub    *hub.Hub
 	logger *zap.Logger
-	stamps *stampRepository
+	repository.StampRepository
 }
 
 // NewGormRepository リポジトリ実装を初期化して生成します。
 // スキーマが初期化された場合、init: true を返します。
 func NewGormRepository(db *gorm.DB, hub *hub.Hub, logger *zap.Logger, doMigration bool) (repo repository.Repository, init bool, err error) {
 	repo = &Repository{
-		db:     db,
-		hub:    hub,
-		logger: logger.Named("repository"),
-		stamps: makeStampRepository(db),
+		db:              db,
+		hub:             hub,
+		logger:          logger.Named("repository"),
+		StampRepository: makeStampRepository(db, hub),
 	}
 	if doMigration {
 		if init, err = migration.Migrate(db); err != nil {
