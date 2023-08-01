282a84d4bfda3b359b95d1c9a15cac331936f1cc
H1rono <hronok66@gmail.com>
2023-08-01 17:32:27 +0900

    Add test cases `not subscribe USER_GROUP_*`

--- service/bot/handler/ev_user_group_deleted_test.go
+++ service/bot/handler/ev_user_group_deleted_test.go
@@ -54,4 +54,40 @@ func TestUserGroupDeleted(t *testing.T) {
 			"group":    group,
 		}))
 	})
+
+	t.Run("not subscribe USER_GROUP_DELETED", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		handlerCtx := mock_handler.NewMockContext(ctrl)
+		b2 := &model.Bot{
+			ID:              uuid.NewV3(uuid.Nil, "b2"),
+			BotUserID:       uuid.NewV3(uuid.Nil, "bu2"),
+			SubscribeEvents: model.BotEventTypesFromArray([]string{event.MessageCreated.String()}),
+			State:           model.BotActive,
+		}
+		registerBot(t, handlerCtx, b)
+		registerBot(t, handlerCtx, b2)
+
+		user := &model.User{
+			ID:     uuid.NewV3(uuid.Nil, "u"),
+			Name:   "new_user",
+			Status: model.UserAccountStatusActive,
+			Bot:    false,
+		}
+		group := model.UserGroup{
+			ID:          uuid.NewV3(uuid.Nil, "g"),
+			Name:        "new_group",
+			Description: "new_group_description",
+			Type:        "new_group_type",
+		}
+		group.Admins = append(group.Admins, &model.UserGroupAdmin{GroupID: group.ID, UserID: user.ID})
+		group.Members = append(group.Members, &model.UserGroupMember{GroupID: group.ID, UserID: user.ID})
+		et := time.Now()
+
+		expectMulticast(handlerCtx, event.UserGroupDeleted, payload.MakeUserGroupDeleted(et, group), []*model.Bot{b})
+		assert.NoError(t, UserGroupDeleted(handlerCtx, et, intevent.UserGroupDeleted, hub.Fields{
+			"group_id": group.ID,
+			"group":    group,
+		}))
+	})
 }
--- service/bot/handler/ev_user_group_updated_test.go
+++ service/bot/handler/ev_user_group_updated_test.go
@@ -53,4 +53,39 @@ func TestUserGroupUpdated(t *testing.T) {
 			"group_id": group.ID,
 		}))
 	})
+
+	t.Run("not subscribe USER_GROUP_UPDATED", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		handlerCtx := mock_handler.NewMockContext(ctrl)
+		b2 := &model.Bot{
+			ID:              uuid.NewV3(uuid.Nil, "b2"),
+			BotUserID:       uuid.NewV3(uuid.Nil, "bu2"),
+			SubscribeEvents: model.BotEventTypesFromArray([]string{event.MessageCreated.String()}),
+			State:           model.BotActive,
+		}
+		registerBot(t, handlerCtx, b)
+		registerBot(t, handlerCtx, b2)
+
+		user := &model.User{
+			ID:     uuid.NewV3(uuid.Nil, "u"),
+			Name:   "new_user",
+			Status: model.UserAccountStatusActive,
+			Bot:    false,
+		}
+		group := model.UserGroup{
+			ID:          uuid.NewV3(uuid.Nil, "g"),
+			Name:        "new_group",
+			Description: "new_group_description",
+			Type:        "new_group_type",
+		}
+		group.Admins = append(group.Admins, &model.UserGroupAdmin{GroupID: group.ID, UserID: user.ID})
+		group.Members = append(group.Members, &model.UserGroupMember{GroupID: group.ID, UserID: user.ID})
+		et := time.Now()
+
+		expectMulticast(handlerCtx, event.UserGroupUpdated, payload.MakeUserGroupUpdated(et, group.ID), []*model.Bot{b})
+		assert.NoError(t, UserGroupUpdated(handlerCtx, et, intevent.UserGroupUpdated, hub.Fields{
+			"group_id": group.ID,
+		}))
+	})
 }
