4bd896f80964baf4d69de5bdb01a165528e08a13
pirosiki197 <lingmukangtai825@gmail.com>
2023-10-17 21:59:56 +0900

    Add StampAlias model

--- model/stamps.go
+++ model/stamps.go
@@ -18,7 +18,8 @@ type Stamp struct {
 	UpdatedAt time.Time      `gorm:"precision:6"                               json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"precision:6"                               json:"-"`
 
-	File *FileMeta `gorm:"constraint:stamps_file_id_files_id_foreign,OnUpdate:CASCADE,OnDelete:NO ACTION;foreignKey:FileID" json:"-"`
+	Aliases []StampAlias `gorm:"constraint:stamps_id_stamp_aliases_stamp_id_foreign,OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:StampID" json:"aliases"`
+	File    *FileMeta    `gorm:"constraint:stamps_file_id_files_id_foreign,OnUpdate:CASCADE,OnDelete:NO ACTION;foreignKey:FileID" json:"-"`
 }
 
 // TableName スタンプテーブル名を取得します
@@ -29,4 +30,11 @@ func (*Stamp) TableName() string {
 // IsSystemStamp システムが作成したスタンプかどうか
 func (s *Stamp) IsSystemStamp() bool {
 	return s.CreatorID == uuid.Nil && s.ID != uuid.Nil && len(s.Name) > 0
+}
+
+type StampAlias struct {
+	ID        uuid.UUID `gorm:"type:char(36);primaryKey"         json:"id"`
+	StampID   uuid.UUID `gorm:"type:char(36);not null"           json:"stampId"`
+	Name      string    `gorm:"type:varchar(32);not null;unique" json:"name"`
+	CreatorID uuid.UUID `gorm:"type:char(36);not null"           json:"creatorId"`
 }
