78f085a9de11d4326d938595512427e51b242218
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-12-11 06:42:39 +0900

    prepare IoReaderToBytes util

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,30 @@
 package utils
 
+import (
+	"bytes"
+	"io"
+)
+
 func Map[T, R any](s []T, mapper func(item T) R) []R {
 	ret := make([]R, len(s))
 	for i := range s {
 		ret[i] = mapper(s[i])
 	}
 	return ret
+}
+
+func IoReaderToBytes(r io.Reader) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(r); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func MustIoReaderToBytes(r io.Reader) []byte {
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(r); err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
 }
