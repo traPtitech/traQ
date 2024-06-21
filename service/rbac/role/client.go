d559a8baba1a94aab7fd8d15dc9036996b663604
kyosu-1 <sho013039@gmail.com>
2024-06-22 07:09:04 +0900

    chore: clientの権限に関する注意事項を記載

--- service/rbac/role/client.go
+++ service/rbac/role/client.go
@@ -7,6 +7,9 @@ import (
 // Client Clientロール (for OAuth2 client credentials grant)
 const Client = "client"
 
+// 自分自身以外の参照系は許可するようにしたいが、https://github.com/traPtitech/traQ/pull/2433#discussion_r1649383346 
+// の事情から許可できる権限が限られる
+// https://github.com/traPtitech/traQ/issues/2463 で権限を増やせるよう対応予定
 var clientPerms = []permission.Permission{
 	permission.GetUser,
 	permission.GetUserTag,
