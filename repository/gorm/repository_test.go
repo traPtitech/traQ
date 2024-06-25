461afdd9368e1d901b5a7ef8bcb076c202f2e02f
24take <takechi12231223@gmail.com>
2024-06-25 17:01:10 +0900

    repositry_test.goでDSNconfigを適用

--- repository/gorm/repository_test.go
+++ repository/gorm/repository_test.go
@@ -16,6 +16,7 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 
+	driverMysql "github.com/go-sql-driver/mysql"
 	"github.com/traPtitech/traQ/migration"
 	"github.com/traPtitech/traQ/model"
 	"github.com/traPtitech/traQ/repository"
@@ -51,13 +52,23 @@ func TestMain(m *testing.M) {
 		common2,
 		common3,
 	}
-	if err := migration.CreateDatabasesIfNotExists("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8mb4&parseTime=true", user, pass, host, port), dbPrefix, dbs...); err != nil {
+	config:= &driverMysql.Config{
+		User:                 user,
+		Passwd:               pass,
+		Net:                  "tcp",
+		Addr:                 fmt.Sprintf("%s:%s", host, port),
+		Collation:            "utf8mb4_general_ci",
+		ParseTime:            true,
+		AllowNativePasswords: true,
+	}
+	if err := migration.CreateDatabasesIfNotExists("mysql", config.FormatDSN(), dbPrefix, dbs...); err != nil {
 		panic(err)
 	}
 
 	for _, key := range dbs {
+		config.DBName = fmt.Sprintf("%s%s", dbPrefix, key)
 		engine, err := gorm.Open(mysql.New(mysql.Config{
-			DSN: fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true", user, pass, host, port, fmt.Sprintf("%s%s", dbPrefix, key)),
+			DSNConfig: config,
 		}))
 		if err != nil {
 			panic(err)
