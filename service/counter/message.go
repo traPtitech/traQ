6dbf8baa36c7192d57a994db01cd90f4b47d0c21
pikachu0310 <pikachu13711@gmail.com>
2024-07-24 07:21:46 +0900

    :bug: panicではなくinitErrorを返すように修正

--- service/counter/message.go
+++ service/counter/message.go
@@ -17,6 +17,7 @@ var (
 	initOnce       sync.Once
 	count          int64
 	messageCounter prometheus.Counter
+	initError      error
 )
 
 // MessageCounter 全メッセージ数カウンタ
@@ -33,7 +34,7 @@ type messageCounterImpl struct {
 }
 
 // NewMessageCounter 全メッセージ数カウンタを生成します
-func NewMessageCounter(db *gorm.DB, hub *hub.Hub) MessageCounter {
+func NewMessageCounter(db *gorm.DB, hub *hub.Hub) (MessageCounter, error) {
 	messageCounter = promauto.NewCounter(prometheus.CounterOpts{
 		Namespace: "traq",
 		Name:      "messages_count_total",
@@ -42,14 +43,19 @@ func NewMessageCounter(db *gorm.DB, hub *hub.Hub) MessageCounter {
 		db:  db,
 		hub: hub,
 	}
-	initOnce.Do(mc.initializeCounter)
-	return mc
+	initOnce.Do(func() {
+		initError = mc.initializeCounter()
+	})
+	if initError != nil {
+		return nil, initError
+	}
+	return mc, nil
 }
 
-func (mc *messageCounterImpl) initializeCounter() {
+func (mc *messageCounterImpl) initializeCounter() error {
 	var initialCount int64
 	if err := mc.db.Unscoped().Model(&model.Message{}).Count(&initialCount).Error; err != nil {
-		panic(err)
+		return err
 	}
 	atomic.StoreInt64(&count, initialCount)
 	messageCounter.Add(float64(initialCount))
@@ -59,15 +65,20 @@ func (mc *messageCounterImpl) initializeCounter() {
 			mc.inc()
 		}
 	}()
+	return nil
 }
 
 func (mc *messageCounterImpl) Get() int64 {
-	initOnce.Do(mc.initializeCounter)
+	initOnce.Do(func() {
+		initError = mc.initializeCounter()
+	})
 	return atomic.LoadInt64(&count)
 }
 
 func (mc *messageCounterImpl) inc() {
-	initOnce.Do(mc.initializeCounter)
+	initOnce.Do(func() {
+		initError = mc.initializeCounter()
+	})
 	atomic.AddInt64(&count, 1)
 	messageCounter.Inc()
 }
