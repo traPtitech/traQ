ba0c0f56746c1c76585a27995c629a96dde08e02
pirosiki197 <lingmukangtai825@gmail.com>
2025-04-25 11:21:13 +0900

    feat: implement generic Set data structure

---
+++ utils/set/set.go
@@ -0,0 +1,44 @@
+package set
+
+import "iter"
+
+type Set[T comparable] struct {
+	set map[T]struct{}
+}
+
+func New[T comparable]() *Set[T] {
+	return &Set[T]{
+		set: make(map[T]struct{}),
+	}
+}
+
+func (set *Set[T]) Add(v ...T) {
+	for _, v := range v {
+		set.set[v] = struct{}{}
+	}
+}
+
+func (set *Set[T]) Remove(v ...T) {
+	for _, v := range v {
+		delete(set.set, v)
+	}
+}
+
+func (set *Set[T]) Contains(v T) bool {
+	_, ok := set.set[v]
+	return ok
+}
+
+func (set *Set[T]) Len() int {
+	return len(set.set)
+}
+
+func (set *Set[T]) Values() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for v := range set.set {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
