57e9446e563a77431188d6d7ba3c21387fa474c9
comavius <comavius@proton.me>
2025-07-25 16:01:59 +0900

    feat: add rate limiting middleware

---
+++ router/middlewares/rate_limit.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/traPtitech/traQ/model"
+	"github.com/traPtitech/traQ/router/consts"
+	"golang.org/x/time/rate"
+)
+
+// RateLimit 各ユーザーについて、すべてのエンドポイントでのリクエスト数を制限するミドルウェア
+func RateLimit(limit rate.Limit) echo.MiddlewareFunc {
+	return middleware.RateLimiterWithConfig(middleware.RateLimiterConfig{
+		Store: middleware.NewRateLimiterMemoryStore(limit),
+		IdentifierExtractor: func(ctx echo.Context) (string, error) {
+			user := ctx.Get(consts.KeyUser).(model.UserInfo)
+			return user.GetID().String(), nil
+		},
+	})
+}
