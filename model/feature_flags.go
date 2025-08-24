e096b13f52927c5cbe74b5f59402ba7592d5003b
tsu-so <marumaru.tsuku@gmail.com>
2025-08-24 18:10:15 +0900

    fix:correct lint, format

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -6,8 +6,8 @@ import (
 
 // FeatureFlag FeatureFlagの管理をする構造体
 type FeatureFlag struct {
-	UserID uuid.UUID `gorm:"type:char(36);not null;primaryKey;"` 
-	FeatureFlagsJson string `gorm:"type:TEXT NOT NULL"`
+	UserID           uuid.UUID `gorm:"type:char(36);not null;primaryKey;"`
+	FeatureFlagsJSON string    `gorm:"type:TEXT NOT NULL"`
 }
 
 // TableName DBの名前を指定
